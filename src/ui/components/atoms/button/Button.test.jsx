f82876f029edf2f35d9c97d1af420eebf2520d31
Ditmar <ditmarcastro@gmail.com>
2024-05-25 10:12:21 -0400

    feat: fixed husky

--- src/ui/components/atoms/button/Button.test.jsx
+++ src/ui/components/atoms/button/Button.test.jsx
@@ -1,17 +1,18 @@
-import {render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Facebook from '../../../../assets/icons/facebook.svg?raw';
 import Google from '../../../../assets/icons/google.svg?raw';
-import Button from './Button';
-describe('Testing Button React Component', () => {
-    test('should be rendere Google', () => {
-        render(<Button icon={Google}>Google</Button>);
-        const buttonElement = screen.getByText(/Google/i);
-        expect(buttonElement).toBeInTheDocument();
-    });
+import Button from './Button.tsx';
 
-    test('should be rendere Facebook', () => {
-        render(<Button icon={Facebook}>Facebook</Button>);
-        const buttonElement = screen.getByText(/Facebook/i);
-        expect(buttonElement).toBeInTheDocument();
-    })
+describe('Testing Button React Component', () => {
+  test('should be rendere Google', () => {
+    render(<Button icon={Google}>Google</Button>);
+    const buttonElement = screen.getByText(/Google/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test('should be rendere Facebook', () => {
+    render(<Button icon={Facebook}>Facebook</Button>);
+    const buttonElement = screen.getByText(/Facebook/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
