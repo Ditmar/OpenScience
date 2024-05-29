ba3eda85518136a6b655141ff46b8d63f0b906f1
Mauricio Elio FLores Herbas <mfloresherbas00@gmail.com>
2024-05-29 00:48:19 -0400

    feature(common):added SEM-040

--- src/ui/components/atoms/publish_button/PublishButton.tsx
+++ src/ui/components/atoms/publish_button/PublishButton.tsx
@@ -5,8 +5,20 @@ function PublishButton(props: IProps) {
   const { children } = props;
   return (
     <div className="buttonPublish">
-      <button className="buttonPublish__button">
-        <span className="buttonPublish__label">{children}</span>
+      <button
+        className="buttonPublish__button"
+        style={{
+          background: '#0793bf',
+        }}
+      >
+        <span
+          className="buttonPublish__label"
+          style={{
+            color: '#fff',
+          }}
+        >
+          {children}
+        </span>
       </button>
     </div>
   );
