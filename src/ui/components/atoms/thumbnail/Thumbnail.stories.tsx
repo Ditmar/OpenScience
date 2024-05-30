34f4c22360a1480d266787de343868a07ae36e07
joigfema <josignfermam@gmail.com>
2024-05-29 20:50:34 -0400

    feature: added SEM-065

---
+++ src/ui/components/atoms/thumbnail/Thumbnail.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Thumbnail from './Thumbnail';
+import Rectangle27 from '../../../../assets/imgs/Rectangle27.png';
+import Rectangle28 from '../../../../assets/imgs/Rectangle28.png';
+import Rectangle29 from '../../../../assets/imgs/Rectangle29.png';
+import Rectangle30 from '../../../../assets/imgs/Rectangle30.png';
+import Rectangle31 from '../../../../assets/imgs/Rectangle31.png';
+import Rectangle32 from '../../../../assets/imgs/Rectangle32.png';
+
+const meta = {
+  title: 'ui/components/atoms/thumbnail',
+  component: Thumbnail,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    pathImage: {
+      control: 'select',
+      options: [Rectangle27, Rectangle28, Rectangle29, Rectangle30, Rectangle31, Rectangle32],
+    },
+    alt: { control: 'text' },
+  },
+} satisfies Meta<typeof Thumbnail>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const VisualThumbnail: Story = {
+  args: {
+    pathImage: String(Rectangle28),
+    alt: 'Rectangle28',
+  },
+};
