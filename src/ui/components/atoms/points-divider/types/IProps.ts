e2bf075b7e4cd8cd84580165906f0f980cf5cb03
DanielaAlejandraMamanisilva <danielaalejandramamanisilva@gmail.com>
2024-05-31 16:20:25 -0400

    add component points divider, SEM-073

---
+++ src/ui/components/atoms/points-divider/types/IProps.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface DividerProps {
+  color?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'solid' | 'dotted' | 'double';
+}
