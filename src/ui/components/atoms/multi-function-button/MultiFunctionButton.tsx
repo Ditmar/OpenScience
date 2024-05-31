9909c72ae8f52696f2f2b8913576973c8e7593de
Camila Delgado <camiladelgado.1321@gmail.com>
2024-05-31 14:41:12 -0400

    feat: added atomic variable button component [SEM-064]

---
+++ src/ui/components/atoms/multi-function-button/MultiFunctionButton.tsx
@@ -0,0 +1,16 @@
+import './styles.scss';
+import type { IProps } from './types/IProps';
+
+function MultiFunctionButton(props: IProps) {
+  const { icon, onClick } = props;
+  return (
+    <button className="variable__button" aria-label="Abrir" onClick={onClick}>
+      <span
+        className="button__icon"
+        data-testid="button-icon"
+        dangerouslySetInnerHTML={{ __html: icon ?? '<div></div>' }}
+      />
+    </button>
+  );
+}
+export default MultiFunctionButton;
