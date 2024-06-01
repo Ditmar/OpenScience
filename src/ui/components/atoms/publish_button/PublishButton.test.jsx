e71579e8c88f196c6f7952cad56a9a27c86df583
Mauricio Flores Herbas <mfloresherbas00@gmail.com>
2024-06-01 13:04:44 -0400

    fix: arrangement of lint.scss SEM-040

---
+++ src/ui/components/atoms/publish_button/PublishButton.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PublishButton from './PublishButton.tsx';
+
+describe('Testing PublishButton React Component', () => {
+  test('should be rendere PUBLICAR ARTICULO', () => {
+    render(<PublishButton>PUBLICAR ARTICULO</PublishButton>);
+    const buttonElement = screen.getByText(/PUBLICAR ARTICULO/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+  test('should have correct styles class', () => {
+    render(<PublishButton>PUBLICAR ARTICULO</PublishButton>);
+    const buttonElement = screen.getByRole('button');
+    const labelElement = screen.getByText(/PUBLICAR ARTICULO/i);
+
+    expect(buttonElement).toHaveClass('buttonPublish__button');
+    expect(labelElement).toHaveClass('buttonPublish__label');
+  });
+});
