7e1ca7c0550ff4d78f6b8fd4209d552194ff426e
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-06-01 12:49:53 -0400

    Actualizar con los últimos cambios

--- src/ui/components/atoms/reference-information/Reference.apa.stories.tsx
+++ src/ui/components/atoms/reference-information/Reference.apa.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react';
 import ReferenceAPA from './Reference.apa';
 
 const meta = {
-  title: 'ui/components/atoms/labelreference',
+  title: 'ui/components/atoms/reference-information',
   component: ReferenceAPA,
   argTypes: {
     referencia: { control: 'text' },
