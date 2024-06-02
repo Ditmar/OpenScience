3251b1d6aac5c4ced03d5f537a097d30c28c6005
MadelenC <madelen02022001@gmail.com>
2024-06-02 18:02:22 -0400

    fear: add container content actualizado 2

--- src/ui/components/atoms/container-content/ContainerContent.stories.tsx
+++ src/ui/components/atoms/container-content/ContainerContent.stories.tsx
@@ -1,8 +1,15 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import ContainerContent from './ContainerContent';
+<<<<<<< HEAD
 import Container from './mock/assets/img/Rectangle 131.svg?raw';
 
 const meta: Meta = {
+=======
+import Container from '../../../../assets/icons/Rectangle 131.svg?raw';
+import '../../../../globals/_variables.scss';
+
+const meta = {
+>>>>>>> 5c170bf (fear: add container content actualizado 2)
   title: 'ui/components/atoms/container-content',
   component: ContainerContent,
   argTypes: {
@@ -10,7 +17,11 @@ const meta: Meta = {
       action: 'click',
     },
   },
+<<<<<<< HEAD
 };
+=======
+} as Meta<typeof ContainerContent>;
+>>>>>>> 5c170bf (fear: add container content actualizado 2)
 
 export default meta;
 
