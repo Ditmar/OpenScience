ef3e06c3d32094de6af38fad96c1d93f3d26e5a2
Ditmar <ditmarcastro@gmail.com>
2024-06-01 22:16:58 -0400

    feat: added basic Docker file deployment

--- style-guide/astro.js
+++ style-guide/astro.js
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:svelte/recommended',
     'plugin:astro/recommended',
   ],
-  ignorePatterns: ['app-server/**'],
+  ignorePatterns: ['app-server/**', '.yarn/**', 'Dockerfile'],
   overrides: [
     {
       files: ['*.astro'],
