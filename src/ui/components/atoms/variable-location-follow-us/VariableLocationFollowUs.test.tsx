a6df3e10deba4799fd24f5f4aea880b6d359ee32
soledad1234567890 <corosoledad66@gmail.com>
2024-06-05 13:27:51 -0400

    feat:modified  added atomic link component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.test.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.test.tsx
@@ -5,26 +5,26 @@ import Facebook from '../../../../assets/icons/facebookk.svg?raw';
 
 describe('Testing Share React Component', () => {
   test('should render Ubicacion', () => {
-    render(<VariableLocationFollowUs icon={Ubicacion} label="UBICACION" />);
+    render(<VariableLocationFollowUs iconlocation={Ubicacion} label="UBICACION" />);
     const labelElement = screen.getByText(/Ubicacion/i);
     expect(labelElement).toBeInTheDocument();
   });
 
   test('should render icon for Ubicacion', () => {
-    render(<VariableLocationFollowUs icon={Ubicacion} label="UBICACION" />);
+    render(<VariableLocationFollowUs iconlocation={Ubicacion} label="UBICACION" />);
     const iconElement = screen.getByTestId('link-icon');
     expect(iconElement).toBeInTheDocument();
   });
 
   test('should render Facebook', () => {
-    render(<VariableLocationFollowUs icon={Facebook} label="FACEBOOK" />);
+    render(<VariableLocationFollowUs iconfacebook={Facebook} label="FACEBOOK" />);
     const labelElement = screen.getByText(/Facebook/i);
     expect(labelElement).toBeInTheDocument();
   });
 
   test('should render icon for Facebook', () => {
-    render(<VariableLocationFollowUs icon={Facebook} label="FACEBOOk" />);
-    const iconElement = screen.getByTestId('link-icon');
+    render(<VariableLocationFollowUs iconfacebook={Facebook} label="FACEBOOk" />);
+    const iconElement = screen.getByTestId('link-iconn');
     expect(iconElement).toBeInTheDocument();
   });
 });
