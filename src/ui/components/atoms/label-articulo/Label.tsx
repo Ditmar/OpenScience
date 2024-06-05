abfe6e1ef0ec66ba359a383b9f3449d7d690a251
Ditmar <spyatorio@gmail.com>
2024-06-05 09:51:57 -0400

    feat: fixed linter

--- src/ui/components/atoms/label-articulo/Label.tsx
+++ src/ui/components/atoms/label-articulo/Label.tsx
@@ -4,6 +4,7 @@ import type { IProps } from './types/IProps';
 function Label(props: IProps) {
   const { children, variant } = props;
   return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label className={`label ${variant ? `label--${variant}` : ''}`}>
       <span className="label__text">{children}</span>
     </label>
