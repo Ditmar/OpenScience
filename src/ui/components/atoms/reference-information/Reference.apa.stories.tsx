2e5cedd2d509a2160544100bfa9ff603f98afc9e
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-06-05 14:20:37 -0400

    cambios corregidos

--- src/ui/components/atoms/reference-information/Reference.apa.stories.tsx
+++ src/ui/components/atoms/reference-information/Reference.apa.stories.tsx
@@ -13,11 +13,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-<<<<<<< HEAD
 export const Default: Story = {
-=======
-export const information: Story = {
->>>>>>> fc94d82221ea4439ad1b21632194440670003051
   args: {
     reference:
       '1. A. Gessain, E. Nakoune, Y. Yazdanpanah, Monkeypox. N. Engl. J. Med. 387, 1783–1793 (2022)',
