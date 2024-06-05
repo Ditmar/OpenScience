4e41afbe7a38ac29b02746c1e65806cc27df69bc
rodrigo slucka <rodrigoslucka@gmail.com>
2024-06-05 14:52:52 -0400

    fix: once again [SEM-058]

--- src/ui/components/atoms/label-share/Share.tsx
+++ src/ui/components/atoms/label-share/Share.tsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 import type { IProps } from './types/IProps';
 
-function Share({ text = 'Compatir', colorVariant = 'primary', icon }: IProps) {
+function Share({ text = 'Compartir', colorVariant = 'primary', icon }: IProps) {
   const getColorClassName = () => {
     return colorVariant === 'main' ? 'label-share--main' : 'label-share--primary';
   };
