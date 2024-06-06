5dfcce3cbf8b4803c73abae5d64e9555a63c35b3
Jerson Elias <128757339+JersonEM@users.noreply.github.com>
2024-06-06 15:57:06 -0400

    2nd commit: fix errors
    fixing some errors find in the code

--- src/components/Breadcrumbs.stories.js
+++ src/components/Breadcrumbs.stories.js
@@ -1,17 +1,13 @@
-
 import Breadcrumbs from './Breadcrumbs.astro';
 import { breadcrumbsMock } from '../mocks/mockData.js';
-
 export default {
   title: 'Components/Breadcrumbs',
   component: Breadcrumbs,
 };
-
 const Template = (args) => ({
   Component: Breadcrumbs,
   props: args,
 });
-
 export const Default = Template.bind({});
 Default.args = {
   breadcrumbs: breadcrumbsMock,
