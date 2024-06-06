1a7ca3a0d96e6ae26418c3b89b543200c28d1cef
Angel <aburgoapaco@gmail.com>
2024-06-06 18:03:32 -0400

    feat: added logo [SEM-036]

---
+++ src/ui/components/atoms/logo/logo.stories.tsx
@@ -0,0 +1,33 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import Logo from './logo';
+import '../../../../globals/_variables.scss';
+import logoImage from './logo.svg';
+
+const meta: Meta<typeof Logo> = {
+  title: 'ui/components/atoms/logo',
+  component: Logo,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    src: logoImage,
+    variant: 'default',
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    src: logoImage,
+    variant: 'primary',
+  },
+};
+export const Secondary: Story = {
+  args: {
+    src: logoImage,
+    variant: 'secondary',
+  },
+};
