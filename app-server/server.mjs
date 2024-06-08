3fcb4bc43c36f729eb2290212ecd6a3299974d22
OrtizAbigail <amoscma469@gmail.com>
2024-06-08 15:50:12 -0400

    feat: maded rebease

--- app-server/server.mjs
+++ app-server/server.mjs
@@ -1,9 +1,14 @@
 import express from 'express';
+// eslint-disable-next-line import/no-unresolved, import/extensions
 import { handler } from '../dist/server/entry.mjs';
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
 if (process.env.ENV === 'local') {
+  // eslint-disable-next-line import/no-extraneous-dependencies, global-require
   require('dotenv').config();
 }
 const app = express();
+// eslint-disable-next-line turbo/no-undeclared-env-vars
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
