b2c9fdfbffcacef757192839038e010d2676ad78
Then-Crimson <miaut16umf@gmail.com>
2024-06-07 20:53:16 -0400

    fixing the typography in feature/SEM-061.

--- src/ui/components/atoms/article-description/ArticleDescription.tsx
+++ src/ui/components/atoms/article-description/ArticleDescription.tsx
@@ -3,7 +3,6 @@ import './styles.scss';
 import type { IProps } from './types/IProps';
 
 function ArticleDescription({ article, variant }: IProps) {
-
   const textStyleMap = {
     bold: 'bold-text',
     italic: 'italic-text',
@@ -11,20 +10,16 @@ function ArticleDescription({ article, variant }: IProps) {
     highlight: 'highlight-text',
   };
 
-
-   let textStyle = 'default-class'; 
-  if (variant!== undefined && variant!== 'default') {
+  let textStyle = 'default-class';
+  if (variant !== undefined && variant !== 'default') {
     textStyle = textStyleMap[variant];
   }
 
   return (
     <div className="article-description">
       <p className={`article-description__text ${textStyle}`}>{article}</p>
-      
     </div>
   );
 }
 
 export default ArticleDescription;
-
-
