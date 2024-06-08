d0d9dc5d5a3931dcb3a70805c294c8d7876f3a89
Estefani <yuditheylb0@gmail.com>
2024-06-08 17:32:37 -0400

    feat: added atomic burguer button component [SEM-067]

---
+++ src/ui/components/atoms/burger-button/BurgerButton.test.jsx
@@ -0,0 +1,13 @@
+import { render, fireEvent } from '@testing-library/react';
+import BurgerButton from './BurgerButton';
+
+test('BurgerButton toggles active class on click', () => {
+  const handleClick = jest.fn();
+  const { getByRole } = render(<BurgerButton active={false} onClick={handleClick} />);
+  const button = getByRole('button');
+  
+  fireEvent.click(button);
+  
+  expect(handleClick).toHaveBeenCalled();
+});
+
