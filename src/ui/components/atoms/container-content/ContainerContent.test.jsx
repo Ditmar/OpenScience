511b48cd5a9d24ab659d0a58522220ae60dabcbb
MadelenC <madelen02022001@gmail.com>
2024-06-08 00:49:44 -0400

    feat:add container content update files
        pick 3315a0a feat:add container content update files

--- src/ui/components/atoms/container-content/ContainerContent.test.jsx
+++ src/ui/components/atoms/container-content/ContainerContent.test.jsx
@@ -1,21 +1,7 @@
-import { render, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-import { fireEvent } from '@storybook/test';
-import ContainerContent, { Container } from './ContainerContent.tsx';
+import { render } from '@testing-library/react';
+import Container from './ContainerContent.tsx';
 
-describe('Testing containerContent React Component', () => {
-  test('should render icon', () => {
-    render(<ContainerContent icon={Container}>Container</ContainerContent>);
-    const iconElement = screen.getByText(/Container/i);
-    expect(iconElement).toBeInTheDocument();
-  });
-
-  test('collapses content when clicked by user', () => {
-    render(<ContainerContent icon={Container}>Container</ContainerContent>);
-    expect(screen.queryByText(/Expanded content/i)).not.toBeInTheDocument();
-    fireEvent.click(screen.getByText(/Container/i));
-    expect(screen.getByText(/Container/i)).toBeInTheDocument();
-    fireEvent.click(screen.getByText(/Container/i));
-    // expect(screen.queryByText(/Container/i)).not.toBeInTheDocument();
-  });
+test('renders container with initial state', async () => {
+  const { container } = render(Container);
+  expect(container).toMatchSnapshot();
 });
