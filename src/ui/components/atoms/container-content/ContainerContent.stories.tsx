3315a0a6e5c1aa3004fd741d26b4b783feebcb67
MadelenC <madelen02022001@gmail.com>
2024-06-08 00:49:44 -0400

    feat:add container content update files

--- src/ui/components/atoms/container-content/ContainerContent.stories.tsx
+++ src/ui/components/atoms/container-content/ContainerContent.stories.tsx
@@ -1,6 +1,7 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import ContainerContent from './ContainerContent';
 import Container from './mock/assets/img/Rectangle 131.svg?raw';
+import Container2 from './mock/assets/img/Rectangle 130.svg?raw';
 
 const meta: Meta = {
   title: 'ui/components/atoms/container-content',
@@ -18,7 +19,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    // children: 'Container',
+    // children: 'Container2',
     icon: Container,
+    icon2: Container2,
   },
 };
--- src/ui/components/atoms/container-content/ContainerContent.tsx
+++ src/ui/components/atoms/container-content/ContainerContent.tsx
@@ -2,16 +2,17 @@ import './styles.scss';
 import type { IProps } from './types/IProps';
 
 function ContainerContent(props: IProps) {
-  const { children, icon } = props;
+  const { icon, icon2 } = props;
   return (
-    <div className="container-content">
-      <div className="container-content-icon">
-        <span
-          className="container-content-icon-image"
-          dangerouslySetInnerHTML={{ __html: icon ?? '<div></div>' }}
-        />
-      </div>
-      <h1>`{children}`</h1>
+    <div className="container-content-icon">
+      <span
+        className="container-content"
+        dangerouslySetInnerHTML={{ __html: icon2 ?? '<div></div>' }}
+      />
+      <span
+        className="container-content-icon-image"
+        dangerouslySetInnerHTML={{ __html: icon ?? '<div></div>' }}
+      />
     </div>
   );
 }
