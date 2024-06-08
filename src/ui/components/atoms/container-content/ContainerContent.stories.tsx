55e0ca5bc4304b6581a8c81092dd5b85a14ed021
MadelenC <madelen02022001@gmail.com>
2024-06-08 13:00:46 -0400

    feature:add container content solved problems

--- src/ui/components/atoms/container-content/ContainerContent.stories.tsx
+++ src/ui/components/atoms/container-content/ContainerContent.stories.tsx
@@ -1,27 +1,9 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import ContainerContent from './ContainerContent';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3315a0a6e5c1aa3004fd741d26b4b783feebcb67
 import Container from './mock/assets/img/Rectangle 131.svg?raw';
 import Container2 from './mock/assets/img/Rectangle 130.svg?raw';
 
 const meta: Meta = {
-<<<<<<< HEAD
-=======
-import Container from '../../../../assets/icons/Rectangle 131.svg?raw';
-
-const meta = {
->>>>>>> 5c170bf (fear: add container content actualizado 2)
-=======
-import Container from './mock/assets/img/Rectangle 131.svg?raw';
-
-const meta: Meta = {
->>>>>>> e7e2d43 (feat: add container content updating img paths)
-=======
->>>>>>> 3315a0a6e5c1aa3004fd741d26b4b783feebcb67
   title: 'ui/components/atoms/container-content',
   component: ContainerContent,
   argTypes: {
@@ -29,19 +11,7 @@ const meta: Meta = {
       action: 'click',
     },
   },
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 };
-=======
-} as Meta<typeof ContainerContent>;
->>>>>>> 5c170bf (fear: add container content actualizado 2)
-=======
-};
->>>>>>> e7e2d43 (feat: add container content updating img paths)
-=======
-};
->>>>>>> 3315a0a6e5c1aa3004fd741d26b4b783feebcb67
 
 export default meta;
 
