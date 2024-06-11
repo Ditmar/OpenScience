87a20f08c737cb62c19dd21894ef64657581df4c
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-06-11 15:19:43 -0400

    mocked agragado

--- src/ui/components/atoms/reference-information/Reference.apa.tsx
+++ src/ui/components/atoms/reference-information/Reference.apa.tsx
@@ -1,42 +1,75 @@
+import React, { useEffect, useState } from 'react';
 import type { IProps } from './types/IProps';
 import './styles.scss';
+
+interface Urls {
+  pnasUrl: string;
+  nejmUrl: string;
+  pubmedUrl: string;
+  googleScholarUrl: string;
+}
 
 function ReferenceAPA(props: IProps) {
   const { text, color } = props;
   const colorText = color ? `reference-apa__text--${color}` : '';
   const colorLinks = color ? `reference-apa__links--${color}` : '';
 
-  const pnasUrl = 'https://www.pnas.org/doi/10.1073/pnas.2301662120#body-ref-r1';
-  const nejmUrl = 'https://www.nejm.org/doi/10.1056/NEJMra2208860';
-  const pubmedUrl = 'https://pubmed.ncbi.nlm.nih.gov/36286263/';
-  const googleScholarUrl = `https://scholar.google.com/scholar_lookup?title=Monkeypox&author=A.+Gessain&
-    author=E.+Nakoune&author=Y.+Yazdanpanah&publication_year=2022&journal=N.+Engl.+J.+Med.&
-    pages=1783-1793&doi=10.1056%2FNEJMra2208860&pmid=36286263`;
+  const [urls, setUrls] = useState<Urls>({
+    pnasUrl: '',
+    nejmUrl: '',
+    pubmedUrl: '',
+    googleScholarUrl: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          '/src/ui/components/atoms/reference-information/_mocked/references.json',
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: Urls = (await response.json()) as Urls; // Explicitly type the response
+        setUrls(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          // Aquí puedes manejar el error de una manera apropiada, como mostrarlo en la UI
+        }
+      }
+    };
+
+    fetchData().catch((error: unknown) => {
+      if (error instanceof Error) {
+        // Aquí puedes manejar el error de una manera apropiada, como mostrarlo en la UI
+      }
+    }); // Manejar explícitamente el error con .catch
+  }, []);
 
   return (
     <div className={`reference-apa__text ${colorText}`}>
       <span>{text}</span>
       <div className={`reference-apa__links ${colorLinks}`}>
         <a
-          href={pnasUrl}
+          href={urls.pnasUrl}
           className={`reference-apa__link${color ? ` reference-apa__link--${color}` : ''}`}
         >
           Go to reference
         </a>
         <a
-          href={nejmUrl}
+          href={urls.nejmUrl}
           className={`reference-apa__link${color ? ` reference-apa__link--${color}` : ''}`}
         >
           Crossref
         </a>
         <a
-          href={pubmedUrl}
+          href={urls.pubmedUrl}
           className={`reference-apa__link${color ? ` reference-apa__link--${color}` : ''}`}
         >
           PubMed
         </a>
         <a
-          href={googleScholarUrl}
+          href={urls.googleScholarUrl}
           className={`reference-apa__link${color ? ` reference-apa__link--${color}` : ''}`}
         >
           Google Scholar
