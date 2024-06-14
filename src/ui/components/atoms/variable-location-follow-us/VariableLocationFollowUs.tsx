dfa52f169bdae3635b12764e124a60cea0689b4c
soledad1234567890 <corosoledad66@gmail.com>
2024-06-13 21:29:00 -0400

    feat: update  atomic link location  component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
@@ -6,26 +6,27 @@ function VariableLocationFollowUs(props: IProps) {
   return (
     <div className="location">
       <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">
-        <span className="link__label">{label}</span>
-        <span
-          className="link__icon"
-          data-testid="link-icon"
-          dangerouslySetInnerHTML={{ __html: iconlocation ?? '<div></div>' }}
-        />
-        <img src={pathImage} alt={alt} />
-        <a
-          href="https://www.facebook.com/tuPaginaDeFacebook"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="link__label">{label1}</span>
-          <span
-            className="link__icon"
-            data-testid="link-iconn"
-            dangerouslySetInnerHTML={{ __html: iconfacebook ?? '<div></div>' }}
-          />
-        </a>
+        <span className="link__label">{label} </span>{' '}
       </a>
+      <span
+        className="link__icon"
+        data-testid="link-icon"
+        dangerouslySetInnerHTML={{ __html: iconlocation ?? '<div></div>' }}
+      />
+      <img src={pathImage} alt={alt} />
+      <a
+        href="https://www.facebook.com/tuPaginaDeFacebook"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link__label"
+      >
+        <span className="link__label">{label1} </span>
+      </a>
+      <span
+        className="link__icon"
+        data-testid="link-iconn"
+        dangerouslySetInnerHTML={{ __html: iconfacebook ?? '<div></div>' }}
+      />
     </div>
   );
 }
