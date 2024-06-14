b6c44e9865ebbe0394f8e3fc61ca4333535658df
soledad1234567890 <corosoledad66@gmail.com>
2024-06-13 21:29:00 -0400

    feat: update  atomic link location  component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.stories.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.stories.tsx
@@ -2,29 +2,27 @@ import type { StoryObj, Meta } from '@storybook/react';
 import VariableLocationFollowUs from './VariableLocationFollowUs';
 import Ubicacion from '../../../../assets/icons/ubicacion.svg?raw';
 import Facebook from '../../../../assets/icons/facebookk.svg?raw';
-import Rectangle168 from './mock/img/Rectangle168.png';
+import Rectangle168 from './resources/images/Rectangle168.png';
 
 
 const meta = {
   title: 'ui/components/atoms/variable-location-follow-us',
   component: VariableLocationFollowUs,
   argTypes: {
-    pathImage: {
-      control: 'select',
-    },
+    pathImage: {},
   },
 } as Meta<typeof VariableLocationFollowUs>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-export const Location: Story = {
+export const Primary: Story = {
   args: {
     label: 'UBICACION',
+    label1: 'SEGUINOS EN',
     iconlocation: Ubicacion,
     pathImage: String(Rectangle168),
     alt: 'Rectangle168',
     iconfacebook: Facebook,
-    label1: 'SIGUENOS EN',
   },
 };
