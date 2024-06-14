844a4db4c6cdbb96823676089a7321b61f8f602a
Ditmar <ditmarcastro@gmail.com>
2024-06-14 01:10:13 -0400

    feat: fixed test an propagates dta-testid

--- src/ui/components/atoms/logo-footer/LogoFooter.tsx
+++ src/ui/components/atoms/logo-footer/LogoFooter.tsx
@@ -1,6 +1,5 @@
 import './styles.scss';
-// eslint-disable-next-line import/no-unresolved
-import { Icon } from 'ui/utils/svg-icons/icons';
+import { Icon } from '../../../utils/svg-icons/icons';
 import type { IProps } from './types/IProps';
 
 function LogoFooter(props: IProps) {
@@ -8,7 +7,9 @@ function LogoFooter(props: IProps) {
   const colorFont = `logo--${color as string}`;
 
   return (
-    <div className={`logo ${colorFont}`}>{icon && <Icon src={icon} className="logo-icon" />}</div>
+    <div className={`logo ${colorFont}`}>
+      {icon && <Icon data-testid="logo_icon" src={icon} className="logo-icon" />}
+    </div>
   );
 }
 export default LogoFooter;
