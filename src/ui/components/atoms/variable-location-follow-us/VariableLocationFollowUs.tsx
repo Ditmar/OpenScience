11acb9dd1cd3a4e771a24c966b7d90e1f0db5736
soledad1234567890 <corosoledad66@gmail.com>
2024-06-15 14:19:56 -0400

    feat: update  atomic link location  component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
@@ -2,26 +2,18 @@ import './styles.scss';
 import type { IProps } from './types/IProps';
 
 function VariableLocationFollowUs(props: IProps) {
-  const { label, label1, iconlocation, iconfacebook, pathImage, alt } = props;
+  const { label, label1, iconlocation, iconfacebook, urllocation, urlfacebok, pathImage, alt } =
+    props;
   return (
     <div className="location">
-      <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">
-        <span className="link__label">{label} </span>{' '}
-      </a>
+      <a href={urllocation}>{label} </a>
       <span
         className="link__icon"
         data-testid="link-icon"
         dangerouslySetInnerHTML={{ __html: iconlocation ?? '<div></div>' }}
       />
       <img src={pathImage} alt={alt} />
-      <a
-        href="https://www.facebook.com/tuPaginaDeFacebook"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="link__label"
-      >
-        <span className="link__label">{label1} </span>
-      </a>
+      <a href={urlfacebok}> {label1} </a>
       <span
         className="link__icon"
         data-testid="link-iconn"
