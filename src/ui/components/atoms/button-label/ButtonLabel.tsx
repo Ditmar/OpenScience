fdb12c82ddb7d435bf0ce440e4c91bafaf40540c
Rodrigo Slucka <rodrigoslucka@gmail.com>
2024-06-15 10:13:41 -0400

    fix: Refactor de SVG en OpenScience [SEM-211]

--- src/ui/components/atoms/button-label/ButtonLabel.tsx
+++ src/ui/components/atoms/button-label/ButtonLabel.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './ButtonLabel.scss';
 import type { IProps } from './types/IProps';
+import { Icon } from '../../../utils/svg-icons/icons';
 
 function ButtonLabel({ children, variant = 'primary', icon }: IProps) {
+  const colorFont = `icon--${variant as string}`;
   return (
     <div className="label-icon__container">
-      <span
-        className={`icon icon--${variant}`}
-        dangerouslySetInnerHTML={{ __html: icon ?? '<div></div>' }}
-      />
+      {icon && <Icon data-testid="logo_icon" src={icon} className={`icon icon-- ${colorFont}`}/>}
       <span className="label">{children}</span>
     </div>
   );
