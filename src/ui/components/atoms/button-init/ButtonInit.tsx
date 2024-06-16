52596d4287617e6f0afc51305751715f4eb48a37
arguetdanitzaarmijo <arguetgd79@gmail.com>
2024-06-15 20:10:20 -0400

    feat: correction and added button icon [SEM-047]

--- src/ui/components/atoms/button-init/ButtonInit.tsx
+++ src/ui/components/atoms/button-init/ButtonInit.tsx
@@ -2,12 +2,16 @@ import type { IProps } from './types/IProps';
 import './styles.scss';
 
 function ButtonInit(props: IProps) {
-  const { children, varianButton = 'primary' } = props;
+  const { children, icon, varianButton = 'primary' } = props;
   const button = `button__init--${varianButton}`;
 
   return (
     <button className={`button__init ${button}`}>
       <span>{children}</span>
+      <span
+        className="button__icon--before"
+        dangerouslySetInnerHTML={{ __html: icon ?? '<div></div>' }}
+      />
     </button>
   );
 }
