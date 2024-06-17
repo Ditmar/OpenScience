940a1614c1965a42be01717944b9a4f021f58acd
angel <aburgoapaco@gmail.com>
2024-06-16 21:52:02 -0400

    Corrected test

--- src/ui/components/atoms/logo/types/IProps.ts
+++ src/ui/components/atoms/logo/types/IProps.ts
@@ -2,9 +2,8 @@ import React from 'react';
 
 export interface IProps {
   children: React.ReactNode;
-  src: string;
-  text: string;
-
+  src?: string;
+  text?: string;
   alt?: string;
   icon?: string;
   variant: 'default' | 'primary' | 'secondary';
