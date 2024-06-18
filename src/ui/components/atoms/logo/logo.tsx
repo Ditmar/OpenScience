3d84cf1eb9772f3999cc23a32eae9903b29dbe18
angel <aburgoapaco@gmail.com>
2024-06-18 18:54:20 -0400

    Fixed branch removing useless ico SEM/036-bisn

--- src/ui/components/atoms/logo/logo.tsx
+++ src/ui/components/atoms/logo/logo.tsx
@@ -2,13 +2,12 @@ import './style.scss';
 import type { IProps } from './types/IProps';
 
 function Logo(props: IProps) {
-  const { src, alt, children, variant = 'primary', icon } = props;
+  const { src, alt, children, variant = 'primary' } = props;
   const color = `logo-container--${variant}`;
 
   return (
     <header className={`logo-container ${color}`}>
       <div>
-        {icon && <img src={icon} alt="Icon" />}
         <img src={src} alt={alt} className="logo-image" />
         {children}
       </div>
