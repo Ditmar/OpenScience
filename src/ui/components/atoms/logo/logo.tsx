1942523a361baffeef188171b2bfc968889f6600
Ditmar <spyatorio@gmail.com>
2024-06-19 15:26:49 -0400

    Revert "Removing unnecessary elements"
    This reverts commit a6ffaab4fe9aed5e2e09b0785c0748a99328a882.

--- src/ui/components/atoms/logo/logo.tsx
+++ src/ui/components/atoms/logo/logo.tsx
@@ -2,13 +2,14 @@ import './style.scss';
 import type { IProps } from './types/IProps';
 
 function Logo(props: IProps) {
-  const { src, alt, variant = 'primary' } = props;
+  const { src, alt, children, variant = 'primary' } = props;
   const color = `logo-container--${variant}`;
 
   return (
     <header className={`logo-container ${color}`}>
       <div>
         <img src={src} alt={alt} className="logo-image" />
+        {children}
       </div>
       <div />
     </header>
