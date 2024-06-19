8531dc81821c8f4b78da72acff4932ec812e8f0c
Alex Lima Equise <alexlimaequise5@gmail.com>
2024-06-18 21:00:09 -0400

    feat: Test Arrangement

--- src/ui/components/atoms/label-date/LabelDate.test.jsx
+++ src/ui/components/atoms/label-date/LabelDate.test.jsx
@@ -10,12 +10,12 @@ describe('Testing React LabelDate Component', () => {
   });
   test('Must render Label Date 2', () => {
     render(<LabelDate month="April" day={20} year={2018} />);
-    const labelElement = screen.getAllByText('April 20 | 2018');
+    const labelElement = screen.getByText('April 20 | 2018');
     expect(labelElement).toBeInTheDocument();
   });
   test('Must render Label Date 3', () => {
     render(<LabelDate month="December" day={1} year={2022} />);
-    const labelElement = screen.getAllByText('December 1 | 2022');
+    const labelElement = screen.getByText('December 1 | 2022');
     expect(labelElement).toBeInTheDocument();
   });
 });
