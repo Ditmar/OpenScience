942a3b4f50ea99a9b7f32336686c9ca0172cf731
Ditmar <spyatorio@gmail.com>
2024-06-19 15:26:49 -0400

    Revert "fixing the responsive in feature/SEM-036-bis"
    This reverts commit 1d17be58fd8eed990801064d6b9cd850ee24b971.

--- src/ui/components/atoms/logo/logo.stories.tsx
+++ src/ui/components/atoms/logo/logo.stories.tsx
@@ -1,4 +1,4 @@
-import type { StoryObj, Meta } from '@storybook/react';
+import type { StoryObj, Meta } from'@storybook/react';
 import Logo from './logo';
 import '../../../../globals/_variables.scss';
 import logoImage from '../../../../assets/icons/logo.svg';
