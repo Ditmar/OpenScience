3772e93948112edab9829fc85d133787dee442fc
Ditmar <spyatorio@gmail.com>
2024-06-19 15:26:49 -0400

    Revert "Fixed branch removing useless ico SEM/036-bisn"
    This reverts commit 3d84cf1eb9772f3999cc23a32eae9903b29dbe18.

--- src/ui/components/atoms/logo/logo.tsx
+++ src/ui/components/atoms/logo/logo.tsx
@@ -2,12 +2,13 @@ import './style.scss';
 import type { IProps } from './types/IProps';
 
 function Logo(props: IProps) {
-  const { src, alt, children, variant = 'primary' } = props;
+  const { src, alt, children, variant = 'primary', icon } = props;
   const color = `logo-container--${variant}`;
 
   return (
     <header className={`logo-container ${color}`}>
       <div>
+        {icon && <img src={icon} alt="Icon" />}
         <img src={src} alt={alt} className="logo-image" />
         {children}
       </div>
