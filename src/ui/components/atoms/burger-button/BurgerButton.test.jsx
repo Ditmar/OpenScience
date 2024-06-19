a11ab461425640a4ce0de8b5f5181c1b1850dff2
Estefani <yuditheylb0@gmail.com>
2024-06-19 13:48:16 -0400

    Add component Button burguer [SEM-077]

--- src/ui/components/atoms/burger-button/BurgerButton.test.jsx
+++ src/ui/components/atoms/burger-button/BurgerButton.test.jsx
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import BurgerButton from './BurgerButton.tsx';
+import BurgerButton from './BurgerButton';
 import IconBurger from '../../../../assets/icons/button-burguer.svg?raw';
 
 describe('Testing BurgerButton React Component', () => {
   test('should render ButtonBurger', () => {
-    render(<BurgerButton icon={IconBurger} />);
-    const buttonElement = screen.getByText('button');
+    render(<BurgerButton icon={IconBurger} variant="button" />);
+    const buttonElement = screen.getByRole('button', { name: /button/i });
     expect(buttonElement).toBeInTheDocument();
   });
 });
