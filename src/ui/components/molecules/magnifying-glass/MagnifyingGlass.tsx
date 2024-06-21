d11a3daaf296eb2c49bcebcc40c6e1a9c2734fca
jose <armandoleonbit25@gmail.com>
2024-06-20 20:34:36 -0400

    Feat: new magnifying glass component fix  [SEM-038]

---
+++ src/ui/components/molecules/magnifying-glass/MagnifyingGlass.tsx
@@ -0,0 +1,30 @@
+import './style.scss';
+import { useState } from 'react';
+import type { IProps } from './types/IProps';
+import { Icon } from '../../../utils/svg-icons/icons';
+
+function MagnifyingGlass(props: IProps) {
+  const { children, icon, onClick } = props;
+
+  const [showMore, setShowMore] = useState(false);
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+  return (
+    <div className="glass">
+      <button onClick={handleMoreClick} className="disguise" aria-label="icon">
+        {icon && <Icon data-testid="button__glass" src={icon} className="button__icons--glass" />}
+      </button>
+      {showMore && <input className="divider" />}
+      {showMore && (
+        <button className="button1" onClick={onClick}>
+          <span data-testid="button__glass1" className="button__label--glass">
+            {children}
+          </span>
+        </button>
+      )}
+    </div>
+  );
+}
+export default MagnifyingGlass;
