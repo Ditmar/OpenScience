6b0d449edaedce95a39b67ff347b656b741419dc
DanielaAlejandraMamanisilva <danielaalejandramamanisilva@gmail.com>
2024-06-21 19:57:29 -0400

    fix lint

--- src/ui/components/molecules/logo-google-facebook/LogoGoogleFacebook.test.jsx
+++ src/ui/components/molecules/logo-google-facebook/LogoGoogleFacebook.test.jsx
@@ -1,12 +1,18 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import LogoGoogleFacebook from './LogoGoogleFacebook';
+import LogoGoogleFacebook from './LogoGoogleFacebook.tsx';
 import GoogleIcon from '../../../../assets/icons/google.svg?raw';
 import FacebookIcon from '../../../../assets/icons/facebook.svg?raw';
 import LogoIcon from '../../../../assets/icons/logo.svg?raw';
 
 describe('LogoGoogleFacebook Component', () => {
   test('should render logo and buttons', () => {
-    render(<LogoGoogleFacebook googleIcon={GoogleIcon} facebookIcon={FacebookIcon} logoIcon={LogoIcon} />);
+    render(
+      <LogoGoogleFacebook
+        googleIcon={GoogleIcon}
+        facebookIcon={FacebookIcon}
+        logoIcon={LogoIcon}
+      />,
+    );
     const googleButton = screen.getByText(/Google/i);
     const facebookButton = screen.getByText(/Facebook/i);
     const logoElement = screen.getByTestId('logo_icon');
@@ -18,7 +24,14 @@ describe('LogoGoogleFacebook Component', () => {
 
   test('should handle Google login click', () => {
     const handleGoogleLogin = jest.fn();
-    render(<LogoGoogleFacebook googleIcon={GoogleIcon} facebookIcon={FacebookIcon} logoIcon={LogoIcon} onGoogleLogin={handleGoogleLogin} />);
+    render(
+      <LogoGoogleFacebook
+        googleIcon={GoogleIcon}
+        facebookIcon={FacebookIcon}
+        logoIcon={LogoIcon}
+        onGoogleLogin={handleGoogleLogin}
+      />,
+    );
     const googleButton = screen.getByText(/Google/i);
     fireEvent.click(googleButton);
     expect(handleGoogleLogin).toHaveBeenCalled();
@@ -26,7 +39,14 @@ describe('LogoGoogleFacebook Component', () => {
 
   test('should handle Facebook login click', () => {
     const handleFacebookLogin = jest.fn();
-    render(<LogoGoogleFacebook googleIcon={GoogleIcon} facebookIcon={FacebookIcon} logoIcon={LogoIcon} onFacebookLogin={handleFacebookLogin} />);
+    render(
+      <LogoGoogleFacebook
+        googleIcon={GoogleIcon}
+        facebookIcon={FacebookIcon}
+        logoIcon={LogoIcon}
+        onFacebookLogin={handleFacebookLogin}
+      />,
+    );
     const facebookButton = screen.getByText(/Facebook/i);
     fireEvent.click(facebookButton);
     expect(handleFacebookLogin).toHaveBeenCalled();
--- src/ui/components/molecules/logo-google-facebook/types/IProps.ts
+++ src/ui/components/molecules/logo-google-facebook/types/IProps.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export interface IProps {
   googleIcon: string;
   facebookIcon: string;
