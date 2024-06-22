488d1e30f4da4ef2353581dc549e2390a0e0d30d
DanielaAlejandraMamanisilva <danielaalejandramamanisilva@gmail.com>
2024-06-22 17:39:07 -0400

    changes

--- src/ui/components/molecules/logo-google-facebook/LogoGoogleFacebook.tsx
+++ src/ui/components/molecules/logo-google-facebook/LogoGoogleFacebook.tsx
@@ -1,29 +1,10 @@
-// LogoGoogleFacebook.tsx
 import type { IProps } from './types/IProps';
 import './styles.scss';
-import SMButton from 'ui/components/atoms/social-media-button/Button';
-import LogoFooter from '../../atoms/logo-footer/LogoFooter';
 
-function LogoGoogleFacebook({
-  googleIcon,
-  facebookIcon,
-  logoIcon,
-  onGoogleLogin,
-  onFacebookLogin,
-}: IProps) {
-  return (
-    <div className="auth-container">
-      <LogoFooter icon={logoIcon} />
-      <div className="auth-buttons">
-        <Button icon={googleIcon} onClick={onGoogleLogin}>
-          Google
-        </Button>
-        <Button icon={facebookIcon} onClick={onFacebookLogin}>
-          Facebook
-        </Button>
-      </div>
-    </div>
-  );
+function LogoGoogleFacebook(props: IProps) {
+  const { children } = props;
+
+  return <div className="logo-google-facebook">{children}</div>;
 }
 
 export default LogoGoogleFacebook;
