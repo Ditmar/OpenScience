0ab7a84a95eac315dc62acead49c08fb0f8cf6f0
Alex Lima Equise <alexlimaequise5@gmail.com>
2024-06-21 22:45:52 -0400

    fix: capital date

--- src/ui/components/atoms/label-date/LabelDate.test.jsx
+++ src/ui/components/atoms/label-date/LabelDate.test.jsx
@@ -5,7 +5,7 @@ import LabelDate from './LabelDate.tsx';
 describe('Testing React LabelDate Component', () => {
   test('Must render Label Date 1', () => {
     render(<LabelDate date={new Date(2022, 0, 5)} />);
-    const labelElement = screen.getByText('enero 5 | 2022');
+    const labelElement = screen.getByText('Enero 5 | 2022');
     expect(labelElement).toBeInTheDocument();
     expect(labelElement).toHaveClass('labeldate');
     expect(labelElement).not.toHaveClass('labeldate--overflow');
@@ -14,7 +14,7 @@ describe('Testing React LabelDate Component', () => {
 
   test('Must render Label Date 2', () => {
     render(<LabelDate date={new Date(2018, 3, 20)} />);
-    const labelElement = screen.getByText('abril 20 | 2018');
+    const labelElement = screen.getByText('Abril 20 | 2018');
     expect(labelElement).toBeInTheDocument();
     expect(labelElement).toHaveClass('labeldate');
     expect(labelElement).not.toHaveClass('labeldate--overflow');
@@ -23,7 +23,7 @@ describe('Testing React LabelDate Component', () => {
 
   test('Must render Label Date 3', () => {
     render(<LabelDate date={new Date(2022, 11, 1)} />);
-    const labelElement = screen.getByText('diciembre 1 | 2022');
+    const labelElement = screen.getByText('Diciembre 1 | 2022');
     expect(labelElement).toBeInTheDocument();
     expect(labelElement).toHaveClass('labeldate');
     expect(labelElement).not.toHaveClass('labeldate--overflow');
--- src/ui/components/atoms/label-date/utils/dateFormatter.ts
+++ src/ui/components/atoms/label-date/utils/dateFormatter.ts
@@ -1,6 +1,7 @@
 export const formatDate = (date: Date): string => {
   const month = date.toLocaleString('es-ES', { month: 'long' });
+  const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
   const day = date.getDate().toString();
   const year = date.getFullYear().toString();
-  return `${month} ${day} | ${year}`;
+  return `${capitalizedMonth} ${day} | ${year}`;
 };
