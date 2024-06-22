cefa18065c2d8f6dafb7118e40a43dc179ed623c
soledad1234567890 <corosoledad66@gmail.com>
2024-06-22 11:54:20 -0400

    feat: update corrected atomic link location  component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
@@ -3,14 +3,13 @@ import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function VariableLocationFollowUs(props: IProps) {
-  const { text, icon, url, divider} =
-    props;
+  const { text, icon, url, divider } = props;
   return (
     <div className="location">
       <a href={url}>{text} </a>
-      {icon && <Icon data-testid="link-icon"  src={icon} className="link__icon" />}
-        <span className="divider">{divider}</span>
-     </div>
+      {icon && <Icon data-testid="link-icon" src={icon} className="link__icon" />}
+      <span className="divider">{divider}</span>
+    </div>
   );
 }
 
