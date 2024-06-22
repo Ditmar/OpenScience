d2c20a468ce92504c65842252545f60ccd4a33eb
Alex Lima Equise <alexlimaequise5@gmail.com>
2024-06-21 20:21:54 -0400

    fix the Storybook

--- src/ui/components/atoms/label-date/LabelDate.stories.tsx
+++ src/ui/components/atoms/label-date/LabelDate.stories.tsx
@@ -4,20 +4,23 @@ import LabelDate from './LabelDate';
 const meta: Meta<typeof LabelDate> = {
   title: 'ui/components/atoms/label-date',
   component: LabelDate,
-  argTypes: {},
+  argTypes: {
+    date: { control: 'date' },
+    overflow: { control: 'boolean' },
+  },
 };
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 const createStory = (date: Date, overflow?: boolean): Story => ({
-  render: (args) => <LabelDate date={args.date} overflow={args.overflow} />,
+  render: (args) => <LabelDate date={new Date(args.date)} overflow={args.overflow} />,
   args: {
     date,
     overflow,
   },
 });
 
-export const LabelDate1 = createStory(new Date(2005, 0, 1));
+export const LabelDate1 = createStory(new Date(2001, 0, 1));
 export const LabelDate2 = createStory(new Date(2018, 3, 30));
 export const LabelDate3 = createStory(new Date(2022, 11, 20));
