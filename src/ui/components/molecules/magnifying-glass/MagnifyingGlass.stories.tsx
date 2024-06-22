e7ca8e5030ce0b2128801eb289ce4446c63fc1e4
jose <armandoleonbit25@gmail.com>
2024-06-22 11:29:52 -0400

    Feat: New magnifying glass component name fix entry [SEM-038]

--- src/ui/components/molecules/magnifying-glass/MagnifyingGlass.stories.tsx
+++ src/ui/components/molecules/magnifying-glass/MagnifyingGlass.stories.tsx
@@ -21,7 +21,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const AgnifyinGlass: Story = {
+export const MagnifyinGlass: Story = {
   args: {
     icon: magnifyingglass,
     variant: 'solid',
