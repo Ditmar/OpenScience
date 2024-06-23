e58aff7cdcdbf0996d55eebc7a06ec9b44639145
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-06-22 22:13:44 -0400

    improved ticket

--- src/ui/components/molecules/resource-and-page-footer/ResourceAndPageFooter.stories.tsx
+++ src/ui/components/molecules/resource-and-page-footer/ResourceAndPageFooter.stories.tsx
@@ -11,60 +11,62 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Desktop: Story = {
+export const Default: Story = {
   args: {
     children: (
       <>
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--resources
-          resources-and-page-footer__section--resources-desktop"
-        >
-          <Variables text="RECURSOS" />
-          <Variables text="ARTÍCULOS" />
-          <Variables text="VOLÚMENES" />
+        <div className="resource-and-page-footer-lineone" />
+
+        <div className="resource-and-page-footer-left">
+          <div className="resource-and-page-footer-title">
+            <Variables text="RECURSOS" />
+          </div>
+          <div className="resource-and-page-footer-content">
+            <Variables text="ARTÍCULOS" />
+            <Variables text="VOLÚMENES" />
+          </div>
         </div>
-        <div className="resource-and-page-footer__separator resources-and-page-footer__separator--desktop" />
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--page
-          resources-and-page-footer__section--page-desktop"
-        >
-          <Variables text="PÁGINA" />
-          <Variables text="UNIPOL.EDU.BO" href="https://www.unipol.edu.bo" />
+
+        <div className="resource-and-page-footer-linetwo" />
+
+        <div className="resource-and-page-footer-rigth">
+          <div className="resource-and-page-footer-title">
+            <Variables text="PAGINAS" />
+          </div>
+          <div className="resource-and-page-footer-content">
+            <Variables text="UNIPOL.EDU.BO" href="https://www.unipol.edu.bo" />
+          </div>
         </div>
       </>
     ),
   },
 };
 
-export const Mobile: Story = {
+export const Colors: Story = {
   args: {
     children: (
       <>
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--resources
-          resources-and-page-footer__section--resources-mobile"
-        >
-          <Variables text="RECURSOS" />
+        <div className="resource-and-page-footer-lineone">|</div>
+
+        <div className="resource-and-page-footer-left">
+          <div className="resource-and-page-footer-title">
+            <Variables text="RECURSOS" colorVariant="primary" />
+          </div>
+          <div className="resource-and-page-footer-content">
+            <Variables text="ARTÍCULOS" colorVariant="secondary" />
+            <Variables text="VOLÚMENES" colorVariant="secondary" />
+          </div>
         </div>
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--page
-          resources-and-page-footer__section--page-mobile"
-        >
-          <Variables text="PÁGINA" />
-        </div>
-        <div className="resource-and-page-footer__separator resources-and-page-footer__separator--mobile" />
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--articles
-          resources-and-page-footer__section--articles-mobile"
-        >
-          <Variables text="ARTÍCULOS" />
-          <Variables text="VOLÚMENES" />
-        </div>
-        <div
-          className="resource-and-page-footer__section resources-and-page-footer__section--unipol
-          resources-and-page-footer__section--unipol-mobile"
-        >
-          <Variables text="UNIPOL.EDU.BO" href="https://www.unipol.edu.bo" />
+
+        <div className="resource-and-page-footer-linetwo" />
+
+        <div className="resource-and-page-footer-rigth">
+          <div className="resource-and-page-footer-title">
+            <Variables text="PAGINAS" colorVariant="primary" />
+          </div>
+          <div className="resource-and-page-footer-content">
+            <Variables text="UNIPOL.EDU.BO" colorVariant="main" href="https://www.unipol.edu.bo" />
+          </div>
         </div>
       </>
     ),
