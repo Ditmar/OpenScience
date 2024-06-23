4581c274f7e585e29b107279dab93dacb7a705ff
Ditmar <ditmarcastro@gmail.com>
2024-06-23 15:00:18 -0400

    feat: removed libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as ViteReact from '@vitejs/plugin-react';
 
 export default defineConfig({
   output: 'server',
@@ -11,13 +9,6 @@ export default defineConfig({
     mode: 'http',
   }),
   vite: {
-    plugins: [
-      ViteReact({
-        bable: {
-          plugins: [['babel-plugin-styled-components', { displayName: true, fileName: false }]],
-        },
-      }),
-    ],
     ssr: {
       noExternal: ['path-to-regexp'],
     },
