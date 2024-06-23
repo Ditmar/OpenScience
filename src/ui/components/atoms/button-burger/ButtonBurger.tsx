d40d35ca390c1d4b8c26372ec0d341311ff0081e
Ditmar <ditmarcastro@gmail.com>
2024-06-23 14:51:13 -0400

    feat: added computed styles

--- src/ui/components/atoms/button-burger/ButtonBurger.tsx
+++ src/ui/components/atoms/button-burger/ButtonBurger.tsx
@@ -1,12 +1,16 @@
-import './ButtonBurger.scss';
+import styles from './ButtonBurger.module.scss';
 import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function ButtonBurger({ icon, color, onClick }: IProps) {
-  const colorFont = `button-icon--${color as string}`;
+  const colorFont = styles[color as keyof typeof styles];
   return (
-    <button className={`button-burger ${colorFont}`} aria-label="Menu" onClick={onClick}>
-      {icon && <Icon data-testid="button-icon" src={icon} className="button-icon" />}
+    <button
+      className={`${styles['button-burger']} ${colorFont}`}
+      aria-label="Menu"
+      onClick={onClick}
+    >
+      {icon && <Icon data-testid="button-icon" src={icon} className={styles['button-icon']} />}
     </button>
   );
 }
