f00115c1935a831f0d8eff03b9cab7ae9f775f7f
soledad1234567890 <corosoledad66@gmail.com>
2024-06-24 14:41:56 -0400

    feat: update my  component [SEM-051]

--- src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
+++ src/ui/components/atoms/variable-location-follow-us/VariableLocationFollowUs.tsx
@@ -1,14 +1,22 @@
 import './styles.scss';
 import type { IProps } from './types/IProps';
+import urls from './_mocked/references.json';
 import { Icon } from '../../../utils/svg-icons/icons';
 
+interface Urls {
+  locationUrl: string;
+  facebookUrl: string;
+}
 function VariableLocationFollowUs(props: IProps) {
-  const { text, icon, url, divider } = props;
+  const { iconlocation, iconfacebook, divider } = props;
+  const referenceUrls: Urls = urls as Urls;
   return (
     <div className="location">
-      <a href={url}>{text} </a>
-      {icon && <Icon data-testid="link-icon" src={icon} className="link__icon" />}
+      <a href={referenceUrls.locationUrl}> UBICACION </a>
+      {iconlocation && <Icon data-testid="link-icon" src={iconlocation} className="link__icon" />}
       <span className="divider">{divider}</span>
+      <a href={referenceUrls.facebookUrl}> SIGUENOS EN </a>
+      {iconfacebook && <Icon data-testid="link_icon" src={iconfacebook} className="link__icon" />}
     </div>
   );
 }
