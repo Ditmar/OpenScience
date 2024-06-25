2b160b7a688c565916127816fcbdf19db90a66eb
jose <armandoleonbit25@gmail.com>
2024-06-25 14:18:22 -0400

    Feat: fix component incon-button [SEM-041]

--- src/ui/components/molecules/share-button/Share.tsx
+++ src/ui/components/molecules/share-button/Share.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import './styles.scss';
 import IconButton from '../../atoms/icon-button/IconButton';
 import Facebook from '../../../../assets/icons/facebook1.svg?raw';
 import Telegram from '../../../../assets/icons/telegram.svg?raw';
 import Whatsapp from '../../../../assets/icons/whatsappp.svg?raw';
 import type { IProps } from './types/IProps';
 
-function Share(props: IProps) {
-  const { onClick } = props;
+function Share({ onClick }: IProps) {
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter' || event.key === ' ') {
       onClick();
     }
   };
   return (
-    <div role="button" tabIndex={0} onClick={onClick} onKeyDown={handleKeyDown} aria-label="Share">
+    <div
+      className="share"
+      role="button"
+      tabIndex={0}
+      onClick={onClick}
+      onKeyDown={handleKeyDown}
+      aria-label="Share"
+    >
       <IconButton icon={Whatsapp} />
-      <br />
       <IconButton icon={Telegram} />
-      <br />
       <IconButton icon={Facebook} />
-      <br />
     </div>
   );
 }
