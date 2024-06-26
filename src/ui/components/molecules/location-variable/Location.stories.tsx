5f042471713db52a7556b43c572deb946f5f29b2
jose <armandoleonbit25@gmail.com>
2024-06-26 10:32:21 -0400

    Feat: new component location-variable [SEM-051]

---
+++ src/ui/components/molecules/location-variable/Location.stories.tsx
@@ -0,0 +1,29 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import Facebook from '../../../../assets/icons/facelocation.svg?raw';
+import location from '../../../../assets/icons/location.svg?raw';
+import Location from './Location';
+
+const meta = {
+  title: 'ui/components/molecules/location-variable',
+  component: Location,
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+      options: ['solid', 'dotted', 'dashed'],
+    },
+  },
+} as Meta<typeof Location>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    icon: location,
+    children: 'UBICACIÓN',
+    variant: 'solid',
+    icon1: Facebook,
+    children1: 'SÍGUENOS EN',
+  },
+};
