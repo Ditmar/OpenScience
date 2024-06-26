597b90de89760258b8041ffaaba0e359e00dc86e
OrtizAbigail <amoscma469@gmail.com>
2024-06-26 01:19:34 -0400

    fead:added molecule[SEM-006]

--- src/ui/components/molecules/featured-article/FeaturedArticle.tsx
+++ src/ui/components/molecules/featured-article/FeaturedArticle.tsx
@@ -1,18 +1,9 @@
 import type { IProps } from './types/IProps.js';
 import './FeaturedArticle.scss';
-import { Icon } from '../../../utils/svg-icons/icons';
 
 function FeaturedArticle(props: IProps) {
-  const { children, icon, color } = props;
-  const colorFont = `variable--${color as string}`;
-
-  return (
-    <div className="featured-article">
-      <span className="featured-article__text">{children}</span>
-      <div className={`featured-article__box ${colorFont}`}>
-        {icon && <Icon src={icon} className="PDF" />}
-      </div>
-    </div>
-  );
+  const { children } = props;
+  return <div className="featured_article_conteiner">{children}</div>;
 }
+
 export default FeaturedArticle;
