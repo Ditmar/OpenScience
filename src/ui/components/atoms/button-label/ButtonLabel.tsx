59aa606e7dbe44fe39da1664daa27b64166d2aa7
DanielaAlejandraMamanisilva <danielaalejandramamanisilva@gmail.com>
2024-06-26 20:05:59 -0400

    fixing component 068 to maintain its shape

--- src/ui/components/atoms/button-label/ButtonLabel.tsx
+++ src/ui/components/atoms/button-label/ButtonLabel.tsx
@@ -4,17 +4,17 @@ import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function ButtonLabel({ children, variant = 'primary', icon }: IProps) {
-  const colorFont = styles[`labelIcon__icon--${variant}`];
+  const colorFont = styles[`label-icon__icon--${variant}`];
   return (
-    <div className={styles.labelIcon__container}>
+    <div className={styles['label-icon__container']}>
       {icon && (
         <Icon
           data-testid="logo_icon"
           src={icon}
-          className={`${styles.labelIcon__icon} ${colorFont}`}
+          className={`${styles['label-icon__icon']} ${colorFont}`}
         />
       )}
-      <span className={styles.labelIcon__label}>{children}</span>
+      <span className={styles['label-icon__label']}>{children}</span>
     </div>
   );
 }
