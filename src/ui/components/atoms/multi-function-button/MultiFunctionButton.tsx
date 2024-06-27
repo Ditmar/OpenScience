4e40297fd5ebda8d55077e0b863e0e1804fea635
OrtizAbigail <amoscma469@gmail.com>
2024-06-27 01:05:50 -0400

    fead:refactor variable and multi-function [SEM-314]

--- src/ui/components/atoms/multi-function-button/MultiFunctionButton.tsx
+++ src/ui/components/atoms/multi-function-button/MultiFunctionButton.tsx
@@ -1,13 +1,18 @@
-import './MultiFunctionButton.scss';
+import React from 'react';
+import styles from './MultiFunctionButton.module.scss';
 import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function MultiFunctionButton(props: IProps) {
   const { icon, onClick, color = 'primary' } = props;
-  const colorFont = `variable__button--${color as string}`;
+  const colorFont = `${styles['variable-button']}--${color as string}`;
   return (
-    <button className={`variable__button ${colorFont}`} aria-label="Abrir" onClick={onClick}>
-      {icon && <Icon data-testid="button-icon" src={icon} className="button__icon" />}
+    <button
+      className={`${styles['variable-button']} ${colorFont}`}
+      aria-label="Abrir"
+      onClick={onClick}
+    >
+      {icon && <Icon data-testid="button-icon" src={icon} className={styles['button-icon']} />}
     </button>
   );
 }
