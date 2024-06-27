801389d13971d449dc013757ada1c59a3024b58d
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-06-27 02:08:52 -0400

    Fixing: refactor_

--- src/ui/components/atoms/divider/Divider.tsx
+++ src/ui/components/atoms/divider/Divider.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Divider.module.scss'; // Importa los estilos como CSS Modules
+import styles from './Divider.module.scss';
 import type { IProps } from './types/IProps';
 
 function Divider(props: IProps) {
