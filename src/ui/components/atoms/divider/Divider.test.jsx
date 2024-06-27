90bb4c1a6336fa91782a83d463b62e19f56e7d01
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-06-27 02:04:27 -0400

    Fixing: refactor

--- src/ui/components/atoms/divider/Divider.test.jsx
+++ src/ui/components/atoms/divider/Divider.test.jsx
@@ -1,22 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Divider from './Divider.tsx';
+import styles from './Divider.module.scss'; // Importa los estilos CSS Modules
 
 describe('Testing Divider React Component', () => {
   test('Should be rendered with a solid style', () => {
     render(<Divider variant="solid" />);
     const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider--solid');
+    expect(dividerElement).toHaveClass(styles['divider--solid']); // Verifica la clase CSS Modules
   });
 
   test('Should be rendered with a dotted style', () => {
     render(<Divider variant="dotted" />);
     const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider--dotted');
+    expect(dividerElement).toHaveClass(styles['divider--dotted']); // Verifica la clase CSS Modules
   });
 
   test('Should be rendered with a dashed style', () => {
     render(<Divider variant="dashed" />);
     const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider--dashed');
+    expect(dividerElement).toHaveClass(styles['divider--dashed']); // Verifica la clase CSS Modules
   });
 });
