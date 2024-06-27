297e9a3bd8e8fcb6b2aff333ab1b855ee421f839
Alex Lima Equise <alexlimaequise5@gmail.com>
2024-06-26 21:54:41 -0400

    feat: first commit

--- src/ui/components/atoms/label-vol/LabelVol.tsx
+++ src/ui/components/atoms/label-vol/LabelVol.tsx
@@ -1,10 +1,10 @@
-import './LabelVol.scss';
 import React from 'react';
+import style from './LabelVol.module.scss';
 import type { IProps } from './types/IProps';
 
 function Volumen({ volumen, id }: IProps) {
   return (
-    <p className="volumen">
+    <p className={style.volumen}>
       {volumen} {id}
     </p>
   );
--- src/ui/components/atoms/logo-footer/LogoFooter.tsx
+++ src/ui/components/atoms/logo-footer/LogoFooter.tsx
@@ -1,15 +1,16 @@
-import './LogoFooter.scss';
+import React from 'react';
+import styles from './LogoFooter.module.scss';
 import { Icon } from '../../../utils/svg-icons/icons';
 import type { IProps } from './types/IProps';
 
-function LogoFooter(props: IProps) {
-  const { icon, color } = props;
-  const colorFont = `logo--${color as string}`;
+function LogoFooter({ icon, color }: IProps) {
+  const colorFont = color === 'secondary' ? styles['logo--secondary'] : '';
 
   return (
-    <div className={`logo ${colorFont}`}>
-      {icon && <Icon data-testid="logo_icon" src={icon} className="logo-icon" />}
+    <div className={`${styles.logo} ${colorFont}`}>
+      {icon && <Icon data-testid="logo_icon" src={icon} className={styles['logo-icon']} />}
     </div>
   );
 }
+
 export default LogoFooter;
