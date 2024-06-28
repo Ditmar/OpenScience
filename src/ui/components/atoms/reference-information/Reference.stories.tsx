0b812a07884d1141b103d15330e4e3f5e872863e
royer <royermartinezperez@gmail.com>
2024-06-28 17:00:16 -0400

    feat: added refactor [SEM-313]

--- src/ui/components/atoms/reference-information/Reference.stories.tsx
+++ src/ui/components/atoms/reference-information/Reference.stories.tsx
@@ -1,12 +1,13 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import ReferenceAPA from './Reference';
+import './Reference.module.scss';
 
 const meta: Meta<typeof ReferenceAPA> = {
   title: 'ui/components/atoms/reference-information',
   component: ReferenceAPA,
   argTypes: {
     text: { control: 'text' },
-    color: { control: { type: 'select', options: ['primary', 'main', 'tertiary'] } },
+    color: { control: { type: 'select', options: ['primary', 'secondary', 'tertiary'] } },
   },
 };
 
@@ -14,19 +15,21 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const primary: Story = {
+export const Primary: Story = {
   args: {
     text: 'A. Gessain, E. Nakoune, Y. Yazdanpanah, Monkeypox. N. Engl. J. Med. 387, 1783–1793 (2022) ',
     color: 'primary',
   },
 };
-export const secondary: Story = {
+
+export const Secondary: Story = {
   args: {
     text: 'A. Gessain, E. Nakoune, Y. Yazdanpanah, Monkeypox. N. Engl. J. Med. 387, 1783–1793 (2022) ',
     color: 'secondary',
   },
 };
-export const tertiary: Story = {
+
+export const Tertiary: Story = {
   args: {
     text: 'A. Gessain, E. Nakoune, Y. Yazdanpanah, Monkeypox. N. Engl. J. Med. 387, 1783–1793 (2022) ',
     color: 'tertiary',
