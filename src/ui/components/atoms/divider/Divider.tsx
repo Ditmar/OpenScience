fc3ea17e4b5449d07245304443637ffe4c5f8bf4
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-06-27 22:37:10 -0400

    fix:refactor with rebase

--- src/ui/components/atoms/divider/Divider.tsx
+++ src/ui/components/atoms/divider/Divider.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import styles from './Divider.module.scss';
+import styles from './Divider.module.scss'; // Importamos los estilos como CSS Modules
 import type { IProps } from './types/IProps';
 
 function Divider(props: IProps) {
   const { variant } = props;
-  return <hr className={`${styles.divider} ${styles[`divider--${variant}`]}`} />;
+  const variantClass = `divider--${variant}`; // Convertimos el variant a BEM
+  return <hr className={styles[variantClass]} />;
 }
 
 export default Divider;
