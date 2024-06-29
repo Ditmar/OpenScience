4aa0e64c84eb9fc9b826bd48c1f5952d8b71f758
Ditmar <ditmarcastro@gmail.com>
2024-06-29 14:44:25 -0400

    feat: fix typo

--- src/ui/components/molecules/experiment-pubsub/receive-component.tsx
+++ src/ui/components/molecules/experiment-pubsub/receive-component.tsx
@@ -5,7 +5,7 @@ import type { BasicMessage } from '@PubSub/messages/messages-model';
 import type { EventEmitter } from '@PubSub/types/types';
 import { useState } from 'react';
 
-export function RecieveComponent() {
+export function ReceiveComponent() {
   const [text, setText] = useState('');
   const broker: SimpleBroker<BasicMessage> = SimpleBroker.getInstance();
   const subscriber: Subscriber<BasicMessage> = new Subscriber<BasicMessage>(broker);
