fa0ee577efdc96292a8685768559bfae51894209
Aldrin Roger Perez Miranda <rogerperez1309@gmail.com>
2024-06-29 13:31:56 -0600

    fix: Refactor VolumeBox component to resolve errors without affecting LabelDate component styling, ensuring fidelity to Figma design

--- src/ui/components/molecules/volume-box/VolumeBox.tsx
+++ src/ui/components/molecules/volume-box/VolumeBox.tsx
@@ -1,11 +1,27 @@
-import type { IProps } from './types/IProps';
+import React from 'react';
 import './VolumeBox.scss';
+import { formatDate } from '../../atoms/label-date/utils/dateFormatter';
+import type { IProps } from './types/IProps';
+import LabelDate from '../../atoms/label-date/LabelDate';
 
-function VolumeBox(props: IProps) {
-  const { children, className = '' } = props;
+function VolumeBox({ children, className = '' }: IProps) {
+  const formatVolumeBoxDate = (date: Date): string => {
+    const formattedDate = formatDate(date);
+    return formattedDate.replace(' | ', '. ');
+  };
+
+  const clonedChildren = React.Children.map(children, (child) => {
+    if (React.isValidElement(child) && React.isValidElement(child) && child.type === LabelDate) {
+      const dateProp = (child.props as { date: Date }).date;
+      const formattedDate = formatVolumeBoxDate(dateProp);
+      return <span className="custom-labeldate">{formattedDate}</span>;
+    }
+    return child;
+  });
+
   return (
     <div className={`volume-box ${className}`}>
-      <div className="content">{children}</div>
+      <div className="content">{clonedChildren}</div>
     </div>
   );
 }
