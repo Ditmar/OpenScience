2308dbfd4479030a5f265cb9b8be15e08c1582a6
Ditmar <ditmarcastro@gmail.com>
2024-06-29 14:41:21 -0400

    feat: added expriment of pub/sub

--- src/ui/components/atoms/button-with-variants/ButtonWithVariants.tsx
+++ src/ui/components/atoms/button-with-variants/ButtonWithVariants.tsx
@@ -1,10 +1,10 @@
 import styles from './ButtonWithVariants.module.scss';
 import type { IProps } from './types/IProps';
 
-function ButtonWithVariants({ children, variant = 'primary' }: IProps) {
+function ButtonWithVariants({ children, variant = 'primary', onClick }: IProps) {
   const variantClass = styles[`button-variant--${variant}`];
   return (
-    <button className={`${styles['button-variant']} ${variantClass}`}>
+    <button className={`${styles['button-variant']} ${variantClass}`} onClick={onClick}>
       <span className={styles['button-variant-label']}>{children}</span>
     </button>
   );
