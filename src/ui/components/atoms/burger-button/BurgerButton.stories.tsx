b7a38dd89b1396feb81b8cba978bfdbb93789e3c
Estefani <yuditheylb0@gmail.com>
2024-07-02 01:31:37 -0400

    modified burger button [SEM-077]

---
+++ src/ui/components/atoms/burger-button/BurgerButton.stories.tsx
@@ -0,0 +1,39 @@
+import type { StoryObj } from '@storybook/react';
+import BurgerButton from './BurgerButton';
+import IconBurger from '../../../../assets/icons/button-burguer.svg?raw';
+import Burger from '../../../../assets/icons/button-burguer1.svg?raw';
+
+const meta = {
+  title: 'ui/components/atoms/burger-bsutton',
+  component: BurgerButton,
+  argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BurgerButton>;
+
+export const Primary: Story = {
+  args: {
+    icon: IconBurger,
+    variant: 'main',
+    color: 'primary',
+    onClick: () => {
+      console.log('Button clicked');
+    },
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    icon: Burger,
+    onClick: () => {
+      console.log('Button clicked');
+    },
+  },
+};
