90ec4173b5c2767ed0333f2ed9a66eb3d15eee14
Jose_Fernandez <joigfema@gmail.com>
2024-07-02 11:52:29 -0400

    feature: fix test

--- src/ui/components/organisms/homepage/HomePage.test.jsx
+++ src/ui/components/organisms/homepage/HomePage.test.jsx
@@ -1,5 +1,3 @@
-// HomePage.test.jsx
-
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HomePage from './HomePage.tsx';
@@ -7,32 +5,20 @@ import HomePage from './HomePage.tsx';
 describe('HomePage Component', () => {
   test('renders featured articles correctly', () => {
     render(<HomePage />);
-
-    // Verificar que el texto "ARTÍCULOS DESTACADOS" esté presente
     const featuredArticlesLabels = screen.getAllByText(/ARTÍCULOS DESTACADOS/i);
     expect(featuredArticlesLabels.length).toBeGreaterThan(0);
-
-    // Verificar que los títulos de los artículos estén presentes
     const articleTitles = screen.getAllByText(
       /Spectral study the visual ecology of pelagic sharks with secondarily expanded habitats./i,
     );
     expect(articleTitles.length).toBeGreaterThan(0);
-
-    // Verificar que las descripciones de los artículos estén presentes
     const articleDescriptions = screen.getAllByText(
       /Spectral study the visual ecology of pelagic sharks with secondarily expanded habitats./i,
     );
     expect(articleDescriptions.length).toBeGreaterThan(0);
-
-    // Verificar que las fechas estén presentes
     const articleDates = screen.getAllByText(/abril 15 | 2023/i);
     expect(articleDates.length).toBeGreaterThan(0);
-
-    // Verificar que los nombres de los autores estén presentes
     const authorNames = screen.getAllByText(/Marco Antonio Lopez/i);
     expect(authorNames.length).toBeGreaterThan(0);
-
-    // Verificar que los botones y otros elementos estén presentes y sean accesibles
     expect(screen.getByRole('button', { name: /publicar articulo/i })).toBeInTheDocument();
     expect(screen.getByAltText(/Thumbnail for/i)).toBeInTheDocument();
   });
