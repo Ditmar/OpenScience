6599e59606cda76c3ecf841315d6ec8b05741eb7
FernandaMar <fernanda.martinez5010@gmail.com>
2024-07-02 00:04:39 -0400

    feat: uploading refactor[SEM-311] corrected 2.

--- src/ui/components/atoms/article-title/ArticleTitle.test.tsx
+++ src/ui/components/atoms/article-title/ArticleTitle.test.tsx
@@ -26,5 +26,4 @@ describe('ArticleTitle', () => {
     expect(titleElement).toHaveClass(styles.articleTitle__text);
     expect(titleElement).toHaveClass(styles['articleTitle__text--secondary']);
   });
-
 });
