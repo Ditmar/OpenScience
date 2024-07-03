3a5a6e3ef28ef9ae2e7839ac7359802046ef8a40
Fernando-MV7 <huviel.fer@gmail.com>
2024-07-03 17:35:11 -0400

    feat: added Recent articles and image [SEM-005]

---
+++ src/ui/components/molecules/recent-articles-image/RecentArticlesImage.tsx
@@ -0,0 +1,18 @@
+import type { IProps } from './types/IProps.js';
+import './styles.scss';
+import Rectangle29 from '../../atoms/thumbnail/__mock__/imgs/Rectangle29.png';
+import Thumbnail from '../../atoms/thumbnail/Thumbnail';
+
+function RecentArticlesImage(props: IProps) {
+  const { children } = props;
+  return (
+    <div className="body">
+      <div className="featured-article">{children}</div>
+      <div className="image">
+        <Thumbnail pathImage={Rectangle29} />
+      </div>
+    </div>
+  );
+}
+
+export default RecentArticlesImage;
