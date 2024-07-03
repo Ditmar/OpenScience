e714b96d65fde89b7e28cab8c35e5b385cdb9a13
Fernando-MV7 <huviel.fer@gmail.com>
2024-07-03 18:17:51 -0400

    fixing the yarn build [SEM-005]

--- src/ui/components/molecules/recent-articles-image/RecentArticlesImage.tsx
+++ src/ui/components/molecules/recent-articles-image/RecentArticlesImage.tsx
@@ -3,13 +3,21 @@ import './styles.scss';
 import Rectangle29 from '../../atoms/thumbnail/__mock__/imgs/Rectangle29.png';
 import Thumbnail from '../../atoms/thumbnail/Thumbnail';
 
+console.log('Rectangle29:', Rectangle29);
+
 function RecentArticlesImage(props: IProps) {
   const { children } = props;
   return (
     <div className="body">
       <div className="featured-article">{children}</div>
       <div className="image">
-        <Thumbnail pathImage={Rectangle29} />
+        <Thumbnail
+          pathImage={
+            (typeof Rectangle29 === 'object' && 'default' in Rectangle29
+              ? Rectangle29.default
+              : Rectangle29) as string
+          }
+        />
       </div>
     </div>
   );
