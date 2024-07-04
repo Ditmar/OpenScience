b219a79bb5ab642cb1715bc7a47aa86318a93de4
AimeAla <usera3463@gmail.com>
2024-07-04 12:28:42 -0400

    feature added config for requests to API int Astro.locals.collections

--- src/types/locals.d.ts
+++ src/types/locals.d.ts
@@ -1,6 +1,21 @@
+export interface ArticlesApiResponse {
+  data: {
+    id: number;
+    attributes: {
+      title: string;
+      date: string;
+      complete_text: string;
+      brief: string;
+      createdAt: string;
+      updatedAt: string;
+      publishedAt: string;
+    };
+  }[];
+}
 export interface Locals {
   API_END_POINT: string;
   API_KEY: string;
   API_BASE_ROUTE: string;
   DOMAIN: string;
+  collections: () => ArticlesApiResponse[];
 }
