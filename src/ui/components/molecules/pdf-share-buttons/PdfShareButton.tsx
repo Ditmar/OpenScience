aef2719b6bebf0312b3fc030b53c09089bec41f2
morelia <moreliana.27.09.2000@gmail.com>
2024-07-05 12:29:40 -0400

    feat: added fix molecule component Pdf/Share Buttons SEM[033]

---
+++ src/ui/components/molecules/pdf-share-buttons/PdfShareButton.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './PdfShareButton.scss';
+import VariableButton from '../../atoms/variable-button/VariableButton';
+import IconButton from '../../atoms/icon-button/IconButton';
+import PdfIcon from '../../../../assets/icons/pdf.svg?raw';
+import LinkIcon from '../../../../assets/icons/share.svg?raw';
+import Whatsapp from '../../../../assets/icons/whatsapp.svg?raw';
+import Facebook from '../../../../assets/icons/facebook1.svg?raw';
+import Telegram from '../../../../assets/icons/telegram.svg?raw';
+
+function PdfShareButton() {
+  const [openIcons, setOpenIcons] = useState(false);
+  return (
+    <div className="pdf-share-buttons">
+      {openIcons && (
+        <div className="share-buttons">
+          <IconButton
+            icon={Whatsapp}
+            onClick={() => {
+              console.log('share by whatsapp clicked');
+            }}
+          />
+          <IconButton
+            icon={Facebook}
+            onClick={() => {
+              console.log('share by facebook clicked');
+            }}
+          />
+          <IconButton
+            icon={Telegram}
+            onClick={() => {
+              console.log('share by telegram clicked');
+            }}
+          />
+        </div>
+      )}
+      <div className="pdf-and-share">
+        <VariableButton
+          icon={PdfIcon}
+          onClick={() => {
+            console.log('open pdf clicked');
+          }}
+        />
+        <VariableButton
+          icon={LinkIcon}
+          onClick={() => {
+            setOpenIcons((prev) => !prev);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PdfShareButton;
