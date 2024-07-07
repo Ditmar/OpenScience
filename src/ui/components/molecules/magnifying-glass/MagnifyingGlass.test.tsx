cb41c60207503fbe95d5eed6febe900245289404
dtapiaparisaca <dtapiaparisaca@gmail.com>
2024-07-07 03:08:19 -0400

    feature: fixed the responsive designe and fix glass magnify [SEM-405]

--- src/ui/components/molecules/magnifying-glass/MagnifyingGlass.test.tsx
+++ src/ui/components/molecules/magnifying-glass/MagnifyingGlass.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import magnifyingglass from '../../../../assets/icons/magnifyingglass.svg?raw';
 import MagnifyingGlass from './MagnifyingGlass';
 
@@ -7,62 +7,5 @@ describe('Testing MagnifyingGlass React Component', () => {
     render(<MagnifyingGlass icon={magnifyingglass} variant="solid" />);
     const buttonElement = screen.getByTestId('button__glass');
     expect(buttonElement).toBeInTheDocument();
-  });
-  test('Testing MagnifyingGlass solid', () => {
-    render(<MagnifyingGlass variant="solid" icon="<svg>...</svg>" />);
-
-    const buttonIcon = screen.getByTestId('button__glass');
-    fireEvent.click(buttonIcon);
-
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider--solid');
-  });
-
-  test('Should display the correct children', () => {
-    render(
-      <MagnifyingGlass variant="solid" icon="<svg>...</svg>">
-        BUSCAR
-      </MagnifyingGlass>,
-    );
-
-    const buttonIcon = screen.getByTestId('button__glass');
-    fireEvent.click(buttonIcon);
-
-    expect(screen.getByText('BUSCAR')).toBeInTheDocument();
-  });
-});
-
-function createMock() {
-  let callCount = 0;
-  const mockFunction = () => {
-    callCount += 1;
-  };
-  mockFunction.callCount = () => callCount;
-  return mockFunction;
-}
-
-describe('Testing Button onClick action', () => {
-  test('onClick callback when icon button is clicked', () => {
-    const onClickMock = createMock();
-    render(<MagnifyingGlass icon={magnifyingglass} variant="solid" onClick={onClickMock} />);
-    const buttonElement = screen.getByTestId('button__glass');
-    fireEvent.click(buttonElement);
-    expect(onClickMock.callCount()).toBe(0);
-  });
-  test('Should display the correct children', () => {
-    const onClickMock = createMock();
-    render(
-      <MagnifyingGlass icon="<svg>...</svg>" variant="solid" onClick={onClickMock}>
-        BUSCAR
-      </MagnifyingGlass>,
-    );
-
-    const buttonIcon = screen.getByTestId('button__glass');
-    fireEvent.click(buttonIcon);
-
-    const buttonElement = screen.getByTestId('button__glass1');
-    expect(screen.getByText('BUSCAR')).toBeInTheDocument();
-    fireEvent.click(buttonElement);
-    expect(onClickMock.callCount()).toBe(1);
   });
 });
--- src/ui/components/molecules/magnifying-glass/MagnifyingGlass.tsx
+++ src/ui/components/molecules/magnifying-glass/MagnifyingGlass.tsx
@@ -1,30 +1,15 @@
 import './style.scss';
-import { useState } from 'react';
 import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function MagnifyingGlass(props: IProps) {
-  const { children, variant, icon, onClick } = props;
+  const { icon, onClick } = props;
 
-  const [showMore, setShowMore] = useState(false);
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
   return (
     <div className="glass">
-      <button onClick={handleMoreClick} className="disguise" aria-label="icon">
+      <button onClick={onClick} className="disguise" aria-label="icon">
         {icon && <Icon data-testid="button__glass" src={icon} className="button__icons--glass" />}
       </button>
-      {showMore && <input className="label__glass" />}
-      {showMore && <hr className={`divider--${variant}`} />}
-      {showMore && (
-        <button className="button1" onClick={onClick}>
-          <span data-testid="button__glass1" className="button__label--glass">
-            {children}
-          </span>
-        </button>
-      )}
     </div>
   );
 }
