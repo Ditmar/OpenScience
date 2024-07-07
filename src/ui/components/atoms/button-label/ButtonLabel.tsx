32723da0693be3b09e330badc8efc01b876e6087
DanielaAlejandraMamanisilva <danielaalejandramamanisilva@gmail.com>
2024-07-07 11:30:04 -0400

    ticket 308, fix the styles

--- src/ui/components/atoms/button-label/ButtonLabel.tsx
+++ src/ui/components/atoms/button-label/ButtonLabel.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './styles.scss';
+import styles from './ButtonLabel.module.scss';
 import type { IProps } from './types/IProps';
 import { Icon } from '../../../utils/svg-icons/icons';
 
 function ButtonLabel({ children, variant = 'primary', icon }: IProps) {
-  const colorFont = `label-icon__icon--${variant as string}`;
+  const colorFont = styles[`label-icon__icon--${variant}`];
   return (
-    <div className="label-icon__container">
+    <div className={styles['label-icon__container']}>
       {icon && (
-        <Icon data-testid="logo_icon" src={icon} className={`label-icon__icon ${colorFont}`} />
+        <Icon data-testid="logo_icon" src={icon} className={`${styles['label-icon__icon']} ${colorFont}`} />
       )}
-      <span className="label-icon__label">{children}</span>
+      <span className={styles['label-icon__label']}>{children}</span>
     </div>
   );
 }
