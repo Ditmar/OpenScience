b2f537488a3a39a1e6efb87f463667fe4828d487
Josse <cooper17john@gmail.com>
2024-12-06 16:05:54 -0400

    added to molecule for banner article

--- src/ui/components/molecules/banner-articles/BannerArticles.test.tsx
+++ src/ui/components/molecules/banner-articles/BannerArticles.test.tsx
@@ -1,19 +1,61 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
-import BannerVolumes from './BannerArticles';
-import Rectangle12 from '../../atoms/hero-banner/__mock__/imgs/Rectangle12.png';
-import LogoSvg from '../../../../assets/icons/logo.svg';
+import BannerArticles from './BannerArticles';
+import type { IProps } from './types/IProps';
 
-describe('YearList', () => {
-  it('should render the correct text button', () => {
+describe('BannerArticles Component', () => {
+  const mockProps: IProps = {
+    textSearch: 'Buscar más artículos',
+    backgroundImage: 'test-image.jpg',
+    logo: 'test-logo.jpg',
+  };
+
+  test('renders Logo, HeroBanner, and NavBar components', () => {
     render(
-      <BannerVolumes
-        textSearch="Buscar"
-        backgroundImage={String(Rectangle12)}
-        logo={String(LogoSvg)}
+      <BannerArticles
+        textSearch={mockProps.textSearch}
+        backgroundImage={mockProps.backgroundImage}
+        logo={mockProps.logo}
       />,
     );
 
-    expect(screen.getByText(/año/i)).toBeInTheDocument();
+    expect(screen.getByAltText('Logotipo de la empresa')).toBeInTheDocument();
+    expect(screen.getByAltText('Rectangle6')).toBeInTheDocument();
+    expect(screen.getByText('Artículos')).toBeInTheDocument();
+    expect(screen.getByText('Inicio')).toBeInTheDocument();
+    expect(screen.getByText('Volúmenes')).toBeInTheDocument();
+  });
+
+  test('renders with provided props', () => {
+    render(
+      <BannerArticles
+        textSearch={mockProps.textSearch}
+        backgroundImage={mockProps.backgroundImage}
+        logo={mockProps.logo}
+      />,
+    );
+
+    const backgroundImage = screen.getByAltText('Rectangle6');
+    expect(backgroundImage).toBeInTheDocument();
+    expect(backgroundImage).toHaveAttribute('src', mockProps.backgroundImage);
+
+    const logo = screen.getByAltText('Logotipo de la empresa');
+    expect(logo).toBeInTheDocument();
+    expect(logo).toHaveAttribute('src', mockProps.logo);
+  });
+
+  test('does not render elements when showMore is false by default', () => {
+    render(
+      <BannerArticles
+        textSearch={mockProps.textSearch}
+        backgroundImage={mockProps.backgroundImage}
+        logo={mockProps.logo}
+      />,
+    );
+
+    expect(screen.queryByRole('textbox')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('button__glass1')).not.toBeInTheDocument();
+
+    const layoutDiv = screen.getByRole('banner');
+    expect(layoutDiv).toBeInTheDocument();
   });
 });
