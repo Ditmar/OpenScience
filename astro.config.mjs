946db569e9e9108743a045372657e5311adaea69
kymer591 <albaroJhaz@gmail.com>
2024-12-06 12:16:16 -0400

    feat: astro-seo implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import sitemap from 'astro-sitemap'; // Importa la integración de sitemap
 import astroI18next from 'astro-i18next';
 
 export default defineConfig({
+  site: 'http://localhost:4321/', // Cambia esto al dominio real en producción
   output: 'server',
-  integrations: [react(), astroI18next()],
+  integrations: [
+    react(),
+    astroI18next(),
+    sitemap(), // Configura automáticamente el sitemap
+  ],
   adapter: node({
-    mode: 'http',
+    mode: 'http', // Configura el adaptador Node.js en modo HTTP
   }),
   vite: {
     ssr: {
-      noExternal: ['path-to-regexp'],
+      noExternal: ['path-to-regexp', 'astro-seo'], // Incluye dependencias necesarias
     },
   },
 });
