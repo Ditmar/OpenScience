6b41c78db283e09b372e349cc6e70f322c0c1703
Manuel Martinez Orcko <martinez.orcko.manuel@gmail.com>
2025-05-09 16:47:35 -0400

    feat: add MyButton component with styles and tests

---
+++ src/ui/components/atoms/MyButton/MyButton.styles.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+  backgroundColor: (theme.palette.primary.main = '#1976d2'),
+  color: (theme.palette.primary.contrastText = '#fff'),
+  '&:hover': {
+    backgroundColor: (theme.palette.primary.dark = '#115293'),
+  },
+}));
