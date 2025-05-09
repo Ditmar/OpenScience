67b94e262caa23c62b182ae1a0ff591739df00fe
Carlos G.Calancha V. <giovannivalverde@hotmail.com>
2025-05-09 19:50:48 -0400

    feat: add lab 2

---
+++ src/ui/components/atoms/MyButton/MyButton.styles.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  '&:hover': {
+    backgroundColor: theme.palette.primary.dark,
+  },
+}));
