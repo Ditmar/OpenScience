77f237490700c5722722a7ce87d0b172aad9b231
Kevin Alexis Rodríguez Condori <101363038+KEVIN-117@users.noreply.github.com>
2025-05-08 22:51:38 -0400

    Feature/frontend 278 (#288)
    * feat: setup MUI integration using SSR
    
    * feat: setup MUI integration using SSR
    
    * feat: setup full cofig for MUI using SSR
    
    * feat: fixed all eslint bugs
    
    * fix: solved everything mentioned in the feedback
    
    * fix: solved everything mentioned in the feedback
    
    ---------
    
    Co-authored-by: KEVIN-117 <krodriguez17@gmail.com>

---
+++ src/style-library/themes/default.ts
@@ -0,0 +1,57 @@
+// theme/lightTheme.js
+import { createTheme } from '@mui/material/styles';
+import tokens from './tokens/tokens.json';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: tokens.colors.primary_main,
+    },
+    secondary: {
+      main: tokens.colors.secondary_main,
+    },
+    error: {
+      main: tokens.colors.danger_main,
+    },
+    warning: {
+      main: tokens.colors.warning_main,
+    },
+    background: {
+      default: tokens.colors.background_main,
+      paper: tokens.colors.main,
+    },
+    text: {
+      primary: tokens.colors.text_main_alpha,
+      secondary: tokens.colors.text_secondary,
+    },
+  },
+  typography: {
+    fontFamily: '"Lato", "Roboto", "Montserrat", sans-serif',
+    fontSize: 16,
+    h1: {
+      fontSize: tokens.typography.fontSizes.first,
+      fontWeight: tokens.typography.fontWeights.first,
+    },
+    h2: {
+      fontSize: tokens.typography.fontSizes.quarter,
+      fontWeight: tokens.typography.fontWeights.bold,
+    },
+    h3: {
+      fontSize: tokens.typography.fontSizes.second,
+      fontWeight: tokens.typography.fontWeights.second,
+    },
+    body1: {
+      fontSize: tokens.typography.fontSizes.primary,
+      fontWeight: tokens.typography.fontWeights.bold,
+    },
+    body2: {
+      fontSize: tokens.typography.fontSizes.sixth,
+      color: tokens.colors.text_input,
+    },
+  },
+  spacing: 8,
+  shape: {
+    borderRadius: 4,
+  },
+});
