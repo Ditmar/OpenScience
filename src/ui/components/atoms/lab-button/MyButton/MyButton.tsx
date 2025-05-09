2c1cae7245dfbd3ce7c1c856e23d376f7f1b1842
Fernanda <unoastoraiquemariafernanda@gmail.com>
2025-05-09 17:18:01 -0400

    laboratorio2_seminario

---
+++ src/ui/components/atoms/lab-button/MyButton/MyButton.tsx
@@ -0,0 +1,12 @@
+import { StyledButton } from './MyButton.styles';
+import type { MyButtonProps } from './MyButton.type';
+
+function MyButton({ label, ...props }: MyButtonProps) {
+  return (
+    <StyledButton variant="contained" {...props}>
+      {label}
+    </StyledButton>
+  );
+}
+
+export default MyButton;
