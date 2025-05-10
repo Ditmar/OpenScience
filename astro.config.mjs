35c502d379f998e79f699b761a7371eb22a12f48
KEVIN-117 <krodriguez17@gmail.com>
2025-05-10 15:48:51 -0400

    fix: correct integration vite-plugin-svgr for using SVGs as react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import svgr from 'vite-plugin-svgr';
 import sitemap from 'astro-sitemap';
 import astroI18next from 'astro-i18next';
 
@@ -16,5 +17,49 @@ export default defineConfig({
     ssr: {
       noExternal: ['path-to-regexp', 'astro-seo'],
     },
+    plugins: [
+      svgr({
+        include: '**/*.svg?react',
+        exclude: '',
+        svgrOptions: {
+          icon: true,
+          dimensions: false,
+          runtimeConfig: true,
+          svgProps: {
+            className: 'w-full h-full',
+            fill: 'currentColor',
+            role: 'img',
+            'data-testid': 'icon',
+          },
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'removeAttrs',
+                params: {
+                  attrs: '(data-name|style)',
+                },
+              },
+              {
+                name: 'cleanupIDs',
+                active: true,
+              },
+              {
+                name: 'prefixIds',
+                active: true,
+              },
+              {
+                name: 'removeXMLNS',
+                active: true,
+              },
+            ],
+          },
+          exportType: 'default',
+          expandProps: 'end',
+          ref: true,
+          titleProp: true,
+          descProp: true,
+        },
+      }),
+    ],
   },
 });
