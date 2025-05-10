1b90878918b27430e617bd633b2cbdea7a3a5161
Jerson Elias Mamani <128757339+JersonEM@users.noreply.github.com>
2025-05-09 21:48:11 -0400

    LAB-2JersonEM

---
+++ library/components/atoms/lab-button/MyButton/MyButton.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import MyButton from './Mybutton';
+
+describe('MyButton', () => {
+  it('renders with label', () => {
+    render(<MyButton label="Click me" />);
+    expect(screen.getByText('Click me')).toBeInTheDocument();
+  });
+});
