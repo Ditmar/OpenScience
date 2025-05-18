aa2749a6862fd9775f4fe3e0c35410d37abd38b0
CSunaguaSeminario <101755416+CSunaguaSeminario@users.noreply.github.com>
2025-05-18 04:42:55 -0400

    feat(phone-input): implement phone input component with country code … (#325)
    * feat(phone-input): implement phone input component with country code support and fixes
    
    * fix(phone-input): stylelint errors resolved
    
    * fix(phone-input): stylelint errors resolved2
    
    * fix(phone-input): stylelint errors resolved3
    
    * fix(phone-input): stylelint errors resolved4
    
    * fix(phone-input): stylelint errors resolved5
    
    * fix(phone-input): stylelint errors resolved problem 2
    
    * fix(phone-input): stylelint errors resolved problem 3
    
    * fix(phone-input): stylelint errors resolved problem 6
    
    * fix(phone-input): stylelint errors resolved problem 8
    
    * feat(phone-input): adjust width and layout to improve visual appearance
    
    * arreglando correcciones ticket
    
    * feat(phone-input): implement phone input component with styles, test, and Storybook setup
    
    * feat(phone-input): implement phone input component with styles, test, and Storybook setup 1
    
    * feat(phone-input): implement phone input component with styles, test, and Storybook setup 2
    
    * feat(phone-input): implement phone input component with styles, test, and Storybook setup 3
    
    * feat(phone-input): implement phone input component with styles, test, and Storybook setup 4
    
    * rebase
    
    * arreglando porblemas de rebase
    
    * corrigiendo segundo check de rebase
    
    * solucionando extraccion no deseada
    
    * chore: remove yarn.lock from version control
    
    * solucion
    
    * Add yarn.lock to fix CI dependencies

---
+++ src/ui/components/atoms/phone-input/types/IProps.ts
@@ -0,0 +1,19 @@
+export interface IProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  label?: string;
+  variant?: 'default' | 'focus' | 'error' | 'disabled';
+  size?: 'small' | 'medium' | 'large';
+  borderRadius?: 'none' | 'small' | 'full';
+  showCloseIcon?: boolean;
+  onCloseClick?: () => void;
+  onCopyClick?: () => void;
+  showHintText?: boolean;
+  hintText?: string;
+  className?: string;
+
+  // Nuevo: Código de país
+  countryCode?: string; // Ej: "+591"
+  onCountryChange?: (code: string) => void;
+}
