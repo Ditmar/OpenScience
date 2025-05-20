72ea0924ed674a9bb45d082f1fae0000340e12ad
henrry <henrryademarmamani@gmail.com>
2025-05-20 16:18:28 -0400

    feat(TextCardList): actualizar componente con estilos, prueba y storybook

--- src/ui/components/atoms/TextCardList/TextCardList.stories.tsx
+++ src/ui/components/atoms/TextCardList/TextCardList.stories.tsx
@@ -1,51 +1,76 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import TextCardList from './TextCardList';
-import type { TextCardListContainerProps } from './types/IProps';
 
 const meta: Meta<typeof TextCardList> = {
   title: 'ui/components/atoms/TextCardList',
   component: TextCardList,
-  tags: ['autodocs'],
   argTypes: {
-    className: {
+    variant: {
+      control: 'select',
+      options: ['default', 'info', 'success', 'warning', 'error'],
+    },
+    selected: {
+      control: 'boolean',
+    },
+    onSelect: { action: 'selected' },
+    categoryTag: {
       control: 'text',
-      description: 'Clase adicional para el contenedor del componente',
+    },
+    badge: {
+      control: false,
     },
   },
 };
 
 export default meta;
-
 type Story = StoryObj<typeof TextCardList>;
+
+const baseBadge = {
+  avatarUrl: 'https://i.pravatar.cc/40',
+  text: 'Badge text',
+  trailingText: '100',
+};
+
+const baseArgs = {
+  title: 'Harmonizing Human Experience: The Artistry of UI-UXDesign',
+  description:
+    'Ul is the canvas, UX the brushstroke; Together, they craft an immersive journey where every pixel tells ' +
+    'a story of elegance, efficiency, and effortless delight. Its the artistry of design at its finest.',
+  selected: false,
+  badge: baseBadge,
+};
 
 export const Default: Story = {
   args: {
-    className: '',
-    items: [
-      {
-        id: '1',
-        title: 'Opción 1',
-        description: 'Descripción de la opción 1',
-        selected: false,
-        variant: 'default',
-        categoryTag: 'Etiqueta 1',
-        className: '',
-        onSelect: (selected) => {
-          console.log('Opción 1 seleccionada:', selected);
-        },
-      },
-      {
-        id: '2',
-        title: 'Opción 2',
-        description: 'Descripción de la opción 2',
-        selected: true,
-        variant: 'info',
-        categoryTag: 'Etiqueta 2',
-        className: '',
-        onSelect: (selected) => {
-          console.log('Opción 2 seleccionada:', selected);
-        },
-      },
-    ],
-  } satisfies TextCardListContainerProps,
+    ...baseArgs,
+    variant: 'default',
+  },
+};
+
+export const Info: Story = {
+  args: {
+    ...baseArgs,
+    variant: 'info',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    ...baseArgs,
+    variant: 'success',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    ...baseArgs,
+    variant: 'warning',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    ...baseArgs,
+    variant: 'error',
+  },
 };
