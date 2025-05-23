f2f9f1ddae6c511949a90e5ac76757be7ec1229d
MirianColque <102442877+MirianColque@users.noreply.github.com>
2025-05-23 16:28:51 -0400

    feature(common): add component badges[FRONTEND-249] (#322)
    * feature(common): add component badges[FRONTEND-249]
    
    * feature(common): fix component badges[FRONTEND-249]
    
    * feature(common): fix lint component badges[FRONTEND-249]
    
    * commit to rebasing
    
    * Added component border styling
    
    * add: adding updated colors for the badge component
    
    * fecture(common):Fixing the use of global variables [FRONTEND-249]
    
    * fecture(common):Fixing the use of global variables [FRONTEND-249]
    
    * fecture(common):Fixing the use of global variables [FRONTEND-249]
    
    * fecture(common):Fixing the use of global variables [FRONTEND-249]
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables
    
    * fecture(common):Fixing the use of global variables, bugs eslint
    
    * fecture(common):style improvement FRONTEND-249
    
    * fecture(common):style improvement FRONTEND-249
    
    ---------
    
    Co-authored-by: mirian <miriancolque022@gmail.com>

---
+++ src/ui/components/atoms/badges/badge.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import './badge.scss';
+import Pill from '../../../../library/component/atoms/pills/pills';
+import { IconCircleQuarters, IconClose } from './Icon';
+import type { BadgeProps } from './types/IProps';
+
+function Badge({
+  variant,
+  color,
+  customColor,
+  icon,
+  image,
+  shape = 'default',
+  size = 'md',
+  children,
+  leftCount,
+  rightCount,
+  onClose,
+}: BadgeProps) {
+  const baseClass = 'badge';
+  const variantClass = `badge--${variant}`;
+  const colorClass = `badge--${color}`;
+  const shapeClass = shape === 'rounded' && 'badge--rounded';
+  const sizeClass = `badge--${size}`;
+
+  const getPillColor = (colors: string) => {
+    if (colors === 'neutral') return 'neutral-dark';
+    if (['gray', 'violet', 'blue'].includes(colors)) return 'neutral-light';
+    return 'neutral-light';
+  };
+
+  const getIconColor = () => {
+    if (variant === 'filled') {
+      return color === 'neutral' || color === 'gray' ? 'black' : 'white';
+    }
+
+    if (customColor) return 'currentColor';
+
+    const outlineSoftColors = ['violet', 'blue'];
+    if (outlineSoftColors.includes(color)) {
+      return 'currentColor';
+    }
+
+    return 'black';
+  };
+
+  const iconColor = getIconColor();
+  const iconStyle = { color: iconColor };
+
+  const customStyle =
+    color === 'custom' && customColor
+      ? {
+          backgroundColor: variant === 'filled' ? customColor : 'transparent',
+          borderColor: variant === 'outline' ? customColor : undefined,
+          color: variant === 'outline' || variant === 'soft' ? customColor : undefined,
+        }
+      : {};
+
+  return (
+    <span
+      className={[baseClass, variantClass, colorClass, shapeClass, sizeClass]
+        .filter(Boolean)
+        .join(' ')}
+      style={customStyle}
+    >
+      {onClose && (
+        <button className="badge__close-button" onClick={onClose} aria-label="Close badge">
+          <IconCircleQuarters style={iconStyle} />
+        </button>
+      )}
+
+      {image && <img src={image} alt="avatar" className="badge__image" />}
+      {icon && <span className="badge__icon">{icon}</span>}
+      {leftCount !== undefined && (
+        <Pill text={String(leftCount)} color={getPillColor(color)} variant={variant} size="sm" />
+      )}
+
+      <span className="badge__content">{children}</span>
+
+      {rightCount !== undefined && (
+        <Pill variant={variant} text={String(rightCount)} color={getPillColor(color)} size="sm" />
+      )}
+
+      {onClose && (
+        <button className="badge__close-button" onClick={onClose} aria-label="Close badge">
+          <IconClose style={iconStyle} />
+        </button>
+      )}
+    </span>
+  );
+}
+
+export default Badge;
