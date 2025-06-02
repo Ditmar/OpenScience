d3c9e3243f98f33eecf0bfbf28ae3c52ab7844c8
PatriciaSaavedra <patriciaesaavedramarca@gmail.com>
2025-06-02 02:24:27 -0400

    Revert "feat: initial commit with complete DateTimeInfo component, tests, styles, and documentation"
    This reverts commit c7015525568eba227903f95b44e4da1c201d1368.
    
    	#	modified:   src/ui/utils/vite-svgr/types/IProps.ts

--- src/ui/components/atoms/date-time-info/types/IProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IProps {
-  timestamp: string | Date;
-  showIcon?: boolean;
-  iconPosition?: 'left' | 'right';
-  size?: 'small' | 'medium' | 'large';
-  format?: (ts: string | Date) => string;
-}
