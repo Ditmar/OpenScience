9a885f35317d3c92ac4a7e93abd5935c1f68f782
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2025-06-05 22:06:06 -0400

    feature(refactor):component text field [FRONTEND-348]

---
+++ src/ui/components/molecules/TextFieldWrapper/TextFieldWrapper.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { BaseInput } from '../../atoms/input/BaseInput';
+import { InputLabel } from '../../atoms/label/InputLabel';
+import { HintText } from '../../atoms/hint/HintText';
+import { ValidationIcons } from '../../atoms/icons/ValidationIcons';
+import type { TextFieldWrapperProps } from './types/IProps';
+import './TextFieldWrapper.scss';
+
+export function TextFieldWrapper({
+  id,
+  label,
+  value,
+  placeholder,
+  helperText = '',
+  type = 'text',
+  error = false,
+  disabled = false,
+  status = null,
+  size = 'medium',
+  onChange,
+  className = '',
+}: TextFieldWrapperProps) {
+  return (
+    <Box
+      className={`text-field-wrapper text-field-wrapper--${size} ${
+        status ? `text-field-wrapper__outlined-input--${status}` : ''
+      } ${error ? 'text-field-wrapper__outlined-input--error' : ''} ${
+        disabled ? 'text-field-wrapper__outlined-input--disabled' : ''
+      } ${className}`}
+      display="flex"
+      flexDirection="column"
+      gap={0.5}
+    >
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <BaseInput
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        type={type}
+        error={error}
+        disabled={disabled}
+        size={size} // Passando a prop size para o BaseInput
+        endIcon={<ValidationIcons status={status} />}
+        className="text-field-wrapper__outlined-input"
+        InputProps={{
+          className: 'text-field-wrapper__input-base',
+          classes: {
+            focused: 'text-field-wrapper__outlined-input--focused',
+            error: 'text-field-wrapper__outlined-input--error',
+            disabled: 'text-field-wrapper__outlined-input--disabled',
+          },
+        }}
+      />
+      {helperText && <HintText text={helperText} error={error} id={`${id}-hint`} />}
+    </Box>
+  );
+}
