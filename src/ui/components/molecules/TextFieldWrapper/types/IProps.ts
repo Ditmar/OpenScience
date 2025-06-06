4569854d54d7a97598f9e2d83ac195fac1866cd6
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2025-06-06 00:22:43 -0400

    feature(refactor):component text field [FRONTEND-348]

--- src/ui/components/molecules/TextFieldWrapper/types/IProps.ts
+++ src/ui/components/molecules/TextFieldWrapper/types/IProps.ts
@@ -1,18 +1,17 @@
 import React from 'react';
-import type { TextFieldProps } from '@mui/material';
 
 export interface TextFieldWrapperProps {
   id: string;
   label: string;
   value?: string;
   placeholder?: string;
-  helperText?: string;
-  type?: string;
+  helperText?: string
+  type?: 'text' | 'email' | 'password';
   hint?: string;
   error?: boolean;
   disabled?: boolean;
   status?: 'success' | 'warning' | 'error' | null;
-  size?: TextFieldProps['size']; // Certifique-se que está definido
+  size?: 'small' | 'medium' | 'large';
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
 }
