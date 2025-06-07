b8cf1b867635c93bbdbf978c5f7d72414e131516
Manuel Martinez Orcko <martinez.orcko.manuel@gmail.com>
2025-06-07 10:11:56 -0400

    refactor: improve formatting of SubTitle component for better readability

--- src/ui/components/atoms/subtitle/SubTitle.tsx
+++ src/ui/components/atoms/subtitle/SubTitle.tsx
@@ -2,13 +2,12 @@ import type { IProps } from './types/IProps';
 import styles from './SubTitle.module.scss';
 
 function SubTitle(props: IProps) {
-  const { 
-    text,
-    size = 'lg',
-    weight = 'regular',
-    spacing = 'default',  
-  } = props;
-  return <h2 className={`${styles.subtitle} ${styles[size]} ${styles[weight]} ${styles[spacing]}`}>{text}</h2>;
+  const { text, size = 'lg', weight = 'regular', spacing = 'default' } = props;
+  return (
+    <h2 className={`${styles.subtitle} ${styles[size]} ${styles[weight]} ${styles[spacing]}`}>
+      {text}
+    </h2>
+  );
 }
 
 export default SubTitle;
