2caff12b5ff2528c613172c4997e6d62a4a0d40f
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2025-06-11 23:05:11 -0400

    feature(common):fixing the input and the icons [FRONTEND-348]

--- src/ui/components/atoms/icons/ValidationIcons.tsx
+++ src/ui/components/atoms/icons/ValidationIcons.tsx
@@ -1,10 +1,27 @@
-import { CheckCircleOutline, ErrorOutline } from '@mui/icons-material';
 import React from 'react';
 import type { ValidationIconsProps } from './types/IProps';
 import './ValidationIcons.scss';
+import { Icon } from '../../../utils/vite-svgr/Icon';
 
 export function ValidationIcons({ status }: ValidationIconsProps) {
-  if (status === 'success') return <CheckCircleOutline color="success" />;
-  if (status === 'error') return <ErrorOutline color="error" />;
+  if (status === 'success')
+    return (
+      <Icon
+        iconName="check"
+        strokeWidth="0"
+        width="18"
+        height="18"
+        color="#23c965"
+      />
+    );
+  if (status === 'error')
+    return (
+      <Icon
+        iconName="errors"
+        strokeWidth="0"
+        width="24"
+        height="24"
+      />
+    );
   return null;
 }
