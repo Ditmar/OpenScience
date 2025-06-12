c59a842666b6964783a9f91af5b6ca0452df0856
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2025-06-11 23:09:12 -0400

    feature(common):fixing the input and the icons [FRONTEND-348]

--- src/ui/components/atoms/icons/ValidationIcons.tsx
+++ src/ui/components/atoms/icons/ValidationIcons.tsx
@@ -5,23 +5,7 @@ import { Icon } from '../../../utils/vite-svgr/Icon';
 
 export function ValidationIcons({ status }: ValidationIconsProps) {
   if (status === 'success')
-    return (
-      <Icon
-        iconName="check"
-        strokeWidth="0"
-        width="18"
-        height="18"
-        color="#23c965"
-      />
-    );
-  if (status === 'error')
-    return (
-      <Icon
-        iconName="errors"
-        strokeWidth="0"
-        width="24"
-        height="24"
-      />
-    );
+    return <Icon iconName="check" strokeWidth="0" width="18" height="18" color="#23c965" />;
+  if (status === 'error') return <Icon iconName="errors" strokeWidth="0" width="24" height="24" />;
   return null;
 }
