4d60dcd10731598476fb22b0a42c2d713377674b
Sergio Armando Tacuri Juchani <seryitsarmin@gmail.com>
2025-06-28 13:00:50 -0400

    Line break fixes and storybook path

--- src/ui/components/molecules/DefaultInputs/DefaultInputs.stories.tsx
+++ src/ui/components/molecules/DefaultInputs/DefaultInputs.stories.tsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 import { DefaultInput } from './DefaultInput';
 
 const meta: Meta<typeof DefaultInput> = {
-  title: 'Molecule/DefaultInput',
+  title: 'ui/components/molecules/DefaultInputs',
   component: DefaultInput,
   argTypes: {
     shape: { table: { disable: true } },
