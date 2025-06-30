117e1b21482e6429a8d1f790a75fa85936d98c91
Daniel Delgado <eddelgadocanaza@gmail.com>
2025-06-29 23:59:30 -0400

    feat(TitleSubtitle): Simplify SubtitleTag and TitleTag components using React.createElement and add prop types

--- src/ui/components/atoms/TitleSubtitle/SubtitleTag.tsx
+++ src/ui/components/atoms/TitleSubtitle/SubtitleTag.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import type { SubtitleTagProps } from './types/SubtitleTagProps';
 
 export function SubtitleTag({ tag, className, children }: SubtitleTagProps) {
-  switch (tag) {
-    case 'p':
-      return <p className={className}>{children}</p>;
-    case 'h3':
-      return <h3 className={className}>{children}</h3>;
-    case 'h4':
-      return <h4 className={className}>{children}</h4>;
-    case 'h5':
-      return <h5 className={className}>{children}</h5>;
-    case 'h6':
-      return <h6 className={className}>{children}</h6>;
-    default:
-      return <p className={className}>{children}</p>;
-  }
+  const validTags = ['p', 'h3', 'h4', 'h5', 'h6'] as const;
+
+  const Tag = validTags.includes(tag) ? tag : 'p';
+
+  return React.createElement(Tag, { className }, children);
 }
--- src/ui/components/atoms/TitleSubtitle/TitleTag.tsx
+++ src/ui/components/atoms/TitleSubtitle/TitleTag.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import type { TitleTagProps } from './types/TitleTagProps';
 
 export function TitleTag({ tag, className, children }: TitleTagProps) {
-  switch (tag) {
-    case 'h1':
-      return <h1 className={className}>{children}</h1>;
-    case 'h2':
-      return <h2 className={className}>{children}</h2>;
-    case 'h3':
-      return <h3 className={className}>{children}</h3>;
-    case 'h4':
-      return <h4 className={className}>{children}</h4>;
-    case 'h5':
-      return <h5 className={className}>{children}</h5>;
-    default:
-      return <h2 className={className}>{children}</h2>;
-  }
+  const validTags = ['h1', 'h2', 'h3', 'h4', 'h5'] as const;
+
+  const Tag = validTags.includes(tag) ? tag : 'h2';
+
+  return React.createElement(Tag, { className }, children);
 }
