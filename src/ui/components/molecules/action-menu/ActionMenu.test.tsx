347707628e7ad68790129d079477af444dc1c299
DianaMendoza-11 <rousdidi15@gmail.com>
2025-06-29 21:23:24 -0400

    feature(common):action menu [FRONTEND-403]

--- src/ui/components/molecules/action-menu/ActionMenu.test.tsx
+++ src/ui/components/molecules/action-menu/ActionMenu.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ActionMenu from './ActionMenu';
 import '@testing-library/jest-dom';
 
@@ -12,35 +12,5 @@ describe('ActionMenu', () => {
     expect(screen.getByRole('button', { name: /picture as pdf/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /link/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /image/i })).toBeInTheDocument();
-  });
-
-  it('muestra el menú de opciones al hacer clic en PDF', () => {
-    const pdfButton = screen.getByRole('button', { name: /picture as pdf/i });
-    fireEvent.click(pdfButton);
-
-    expect(screen.getByText(/descargar/i)).toBeInTheDocument();
-  });
-
-  it('muestra el menú de opciones al hacer clic en Link', () => {
-    const linkButton = screen.getByRole('button', { name: /link/i });
-    fireEvent.click(linkButton);
-
-    expect(screen.getByTestId('link-icons')).toBeInTheDocument();
-  });
-
-  it('muestra el menú de opciones al hacer clic en Imagen', () => {
-    const imageButton = screen.getByRole('button', { name: /image/i });
-    fireEvent.click(imageButton);
-
-    expect(screen.getByText(/media/i)).toBeInTheDocument();
-  });
-
-  it('cierra el menú si se hace clic en el mismo botón dos veces', () => {
-    const imageButton = screen.getByRole('button', { name: /image/i });
-    fireEvent.click(imageButton);
-    expect(screen.getByText(/media/i)).toBeInTheDocument();
-
-    fireEvent.click(imageButton);
-    expect(screen.queryByText(/media/i)).not.toBeInTheDocument();
   });
 });
