a0eb16c904f2542922d58b0069bffd2346243f53
Haeintz Mariscal <haeintz.mborja@gmail.com>
2025-07-01 22:07:04 -0400

    fix: Problems in the eslint relationship with OLItem component

--- src/ui/components/atoms/ol-item/OLItem.style.ts
+++ src/ui/components/atoms/ol-item/OLItem.style.ts
@@ -17,7 +17,7 @@ export const OLItemRoot = styled(Box, {
     borderRadius: '0',
   }),
   ...(shape === 'rounded' && {
-    borderRadius: `${(Number(theme.shape.borderRadius) + 4).toString()}px`,
+    borderRadius: theme.shape.borderRadius + 4,
   }),
   ...(shape === 'circle' && {
     borderRadius: '50%',
