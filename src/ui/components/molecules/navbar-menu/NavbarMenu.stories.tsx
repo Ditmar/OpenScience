8a4599ee3ee58ebbcd1aa3ae3281afbbc9d7269d
PatriciaSaavedra <patriciaesaavedramarca@gmail.com>
2025-07-02 22:15:55 -0400

    feat(navbar-menu): changes in the storybook

--- src/ui/components/molecules/navbar-menu/NavbarMenu.stories.tsx
+++ src/ui/components/molecules/navbar-menu/NavbarMenu.stories.tsx
@@ -14,7 +14,7 @@ const meta: Meta<typeof NavbarMenu> = {
   parameters: {
     docs: {
       description: {
-        component: 'Menú de navegación con íconos y texto, accesible y configurable.',
+        component: 'Navbar Menu',
       },
     },
   },
