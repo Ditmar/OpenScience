784da4b15dd6be2782f21fb94f4778484feb8ad5
Sergio Armando Tacuri Juchani <seryitsarmin@gmail.com>
2025-07-03 15:32:47 -0400

    arrangements-TabsSelector

--- src/ui/components/molecules/MenuMedia/MenuMedia.stories.tsx
+++ src/ui/components/molecules/MenuMedia/MenuMedia.stories.tsx
@@ -2,6 +2,15 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { MenuMedia } from './MenuMedia';
 import Rectangle10 from '../../atoms/hero-banner/__mock__/imgs/Rectangle10.png';
 
+const descriptions = [
+  'The recent 2022 international outbreak has caused an unprecedented transmission of the virus mainly in men ' +
+    'that have sex with men. The extent of the outbreak has decreased dramatically after a few months, with 13 to ' +
+    '37 daily cases worldwide in February 2023. We still do not understand the reasons why the MPXV clade circulating ' +
+    'in 2022 was so transmissible in humans.',
+  'Table data is not currently available. Further information will be updated once studies are concluded.',
+  'Additional data and analysis are ongoing. This section will include graphical interpretations, sources, and visual context.',
+];
+
 const meta: Meta<typeof MenuMedia> = {
   title: 'ui/components/molecules/MenuMedia',
   component: MenuMedia,
@@ -17,6 +26,7 @@ const defaultArgs = {
   onOpenImage: () => {
     alert('Imagen abierta');
   },
+  descriptions,
 };
 
 export const Small: Story = {
