61b538b3d5209db0c270c8e33c6cabdaa189283f
Jenny Marilyn Fabian Torrez <marilynfabian27@gmail.com>
2025-07-02 22:36:53 -0400

    Refactored Pill component tests to English and performed rebase to align with updated structure

--- src/style-library/themes/mui.d.ts
+++ src/style-library/themes/mui.d.ts
@@ -7,12 +7,78 @@ declare module '@mui/material/styles' {
       text_light: string;
       stroke_primary_Soft: string;
     };
+
+    pillpalette: {
+      dark: string;
+      dark_soft: string;
+      disabled: string;
+      light: string;
+      negative: string;
+      negative_soft: string;
+      positive: string;
+      positive_soft: string;
+      primary: string;
+      secondary: string;
+      secondary_soft: string;
+      tertiary: string;
+      tertiary_soft: string;
+      warning: string;
+      warning_soft: string;
+      stroke_dark: string;
+      stroke_dark_soft: string;
+      stroke_disabled: string;
+      stroke_negative: string;
+      stroke_negative_soft: string;
+      stroke_positive: string;
+      stroke_positive_soft: string;
+      stroke_primary: string;
+      stroke_primary_soft: string;
+      stroke_secondary: string;
+      stroke_secondary_soft: string;
+      stroke_tertiary: string;
+      stroke_tertiary_soft: string;
+      stroke_warning: string;
+      stroke_warning_soft: string;
+    };
+
   }
   interface PaletteOptions {
     olitemPalette?: {
       stroke_Primary: string;
       text_light: string;
       stroke_primary_Soft: string;
+    };
+    pillpalette?: {
+      dark?: string;
+      dark_soft?: string;
+      disabled?: string;
+      light?: string;
+      negative?: string;
+      negative_soft?: string;
+      positive?: string;
+      positive_soft?: string;
+      primary?: string;
+      secondary?: string;
+      secondary_soft?: string;
+      tertiary?: string;
+      tertiary_soft?: string;
+      warning?: string;
+      warning_soft?: string;
+      stroke_dark?: string;
+      stroke_dark_soft?: string;
+      stroke_disabled?: string;
+      stroke_negative?: string;
+      stroke_negative_soft?: string;
+      stroke_positive?: string;
+      stroke_positive_soft?: string;
+      stroke_primary?: string;
+      stroke_primary_soft?: string;
+      stroke_secondary?: string;
+      stroke_secondary_soft?: string;
+      stroke_tertiary?: string;
+      stroke_tertiary_soft?: string;
+      stroke_warning?: string;
+      stroke_warning_soft?: string;
     };
   }
 
