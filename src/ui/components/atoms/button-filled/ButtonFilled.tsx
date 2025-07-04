57def71fdc928b7783049ab527c4455ec1f56884
ZerGio23 <sergio.apaza1432@gmail.com>
2025-07-03 23:33:45 -0400

    feat: add button structure

--- src/ui/components/atoms/button-filled/ButtonFilled.tsx
+++ src/ui/components/atoms/button-filled/ButtonFilled.tsx
@@ -4,14 +4,16 @@ import { Box, Button } from '@mui/material';
 import { theme } from 'globals/theme';
 
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import { buttonSx } from './ButtonFilled.styles';
 
 export function ButtonFilled(props: IProps) {
-    const { text, textColor, buttonColor, startIcon, endIcon, disabled, borderRadius, size, dark } = props;
+    const { text, startIcon, endIcon, borderRadius, colorVariant, dark, disabled, size, buttonVariant } = props;
 
-    
+    const buttonStyles = buttonSx({ borderRadius, colorVariant, dark, disabled, size, buttonVariant });
+
     return (
         <Box>
-            <Button sx={{backgroundColor: theme.colors.brand.primary[100]}} startIcon={<CancelOutlinedIcon />} endIcon={<CancelOutlinedIcon />}>{text}</Button>
+            <Button sx={buttonStyles} startIcon={<CancelOutlinedIcon />} endIcon={<CancelOutlinedIcon />}>{text}</Button>
         </Box>
     );
 }
