d8424317ef1cd1ce3795e0885960e4dac442d15a
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2025-07-07 17:59:43 -0400

    feature(common):fixing the input writing [FRONTEND-348]

--- src/ui/components/atoms/icons/ValidationIcons.tsx
+++ src/ui/components/atoms/icons/ValidationIcons.tsx
@@ -4,16 +4,22 @@ import './ValidationIcons.scss';
 import { Icon } from '../../../utils/vite-svgr/Icon';
 
 export function ValidationIcons({ status }: ValidationIconsProps) {
-  if (status === 'success')
-    return (
-      <Icon
-        iconName="check"
-        strokeWidth="0"
-        width="18"
-        height="18"
-        color="var(--ads-success-main)"
-      />
-    );
-  if (status === 'error') return <Icon iconName="errors" strokeWidth="0" width="24" height="24" />;
-  return null;
+  switch (status) {
+    case 'success':
+      return (
+        <Icon
+          iconName="check"
+          strokeWidth="0"
+          width="18"
+          height="18"
+          color="var(--ads-success-main)"
+        />
+      );
+
+    case 'error':
+      return <Icon iconName="errors" strokeWidth="0" width="24" height="24" />;
+
+    default:
+      return null;
+  }
 }
